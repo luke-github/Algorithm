dc1fb0acb52d3c021526232012b7dac804adc9cb
Wen-Qi Cao <wenqicao@gmail.com>
2014-06-18 21:31:04 -0400

    init

---
+++ 6_22.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+void printAlgorithm(vector<vector<int> >&vec, int offset);
+
+void printSpiral(vector<vector<int> >& vec){
+	for(int offset=0;offset<ceil(0.5*vec.size());offset++){
+		printAlgorithm(vec,offset);
+	}
+}
+
+void printAlgorithm(vector<vector<int> >&vec, int offset){
+	if((offset*2+1)==vec.size()){
+		cout<<vec[offset][offset];
+	}else{
+		for(int i=offset;i<vec.size()-offset-1;i++)
+			cout<<vec[offset][i];
+		for(int i=offset;i<vec.size()-offset-1;i++)
+			cout<<vec[i][vec.size()-offset-1];
+		for(int i=vec.size()-offset-1;i>offset;i--)
+			cout<<vec[vec.size()-offset-1][i];
+		for(int i=vec.size()-offset-1;i>offset;i--)
+			cout<<vec[i][offset];
+	}
+}
+
+int main(){
+
+}
