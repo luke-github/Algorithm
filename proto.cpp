0cbf6d10fdb58d44a87540554555fce0d2e35e3f
Wen-Qi Cao <wenqicao@gmail.com>
2014-06-19 17:03:14 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -2,46 +2,33 @@
 #include <vector>
 using namespace std;
 
-int stringToInt(string x){
-	if(x=="-"){
-		throw invalid_argument("illegal input");
-	}
-	bool is_negative = x[0]=='-';
-	int result=0;
-	for(int i=is_negative;i<x.size();i++){
-		if(isdigit(x[i])){
-			result=result*10+x[i]-'0';
-		}else{
-			throw invalid_argument("illegal input");
+string modifiedString(string s){
+	int write_index=0;
+	int a_count=0;
+	for(char c : s){
+		if(c!='b'){
+			s[write_index++]=c;
 		}
+		if(c=='a')
+			a_count++;
 	}
-	if(is_negative)
-		result=-result;
-	return result;
+	s.resize(write_index+a_count);
+	int cur_itor=write_index-1;
+	write_index=s.size()-1;
+	while(cur_itor>=0){
+		if(s[cur_itor]=='a'){
+			s[write_index--]='d';
+			s[write_index--]='d';
+		}else{
+			s[write_index--]=s[cur_itor];
+		}
+		cur_itor--;
+	}
+	return s;
 }
 
-string inToString(int x){
-	bool is_negative=false;
-	if(x<0){
-		x=-x;
-		is_negative=true;
-	}
-	string result;
-	while(x){
-		result.push_back('0'+x%10);
-		x/=10;
-	}
-	if(result.empty()){
-		return "0";
-	}
-	if(is_negative)
-		result.push_back('-');
-	reverse(result.begin(),result.end());
-	return result;
-}
+
 
 int main(){
-	int inputint=-123;
-	string inputstr="-1a23";
-	cout<<inToString(inputint)<<" "<<stringToInt(inputstr)<<endl;
+	cout<<modifiedString("abcabc");
 }
