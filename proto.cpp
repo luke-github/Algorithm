1b4e09bf9967f20943ec85365c0baf6c3d12497d
Wen-Qi Cao <wenqicao@gmail.com>
2014-06-20 16:37:06 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
-#include <vector>
+#include <sstream>
 using namespace std;
 
-string sin_string(const string& s){
+string decode(const string& s){
+	int count=0;
 	string result;
-	int index = 1;
-	while(index<s.size()){
-		result += s[index];
-		index += 2;
+	for(char c : s){
+		if(isdigit(c)){
+			count=count*10+c-'0';
+		}else{
+			result.append(count,c);
+			count=0;
+		}
 	}
+	return result;
+}
+
+string encode(const string& s){
+	int count=1;
+	stringstream result;
+	for(int i=1;i<s.size();i++){
+		if(s[i]==s[i-1]){
+			count++;
+		}else{
+			result<<count<<s[i-1];
+			count=1;
+		}
+	}
+	result<<count<<s.back();
+	return result.str();
+}
+
+int main(){
+	string input = "aaabbbbbcc";
+	cout<<encode(input)<<endl;
+	cout<<decode(encode(input));
 }
