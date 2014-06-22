d1fcc598e6d90a26bc512c3df5a0f966c138d085
Wen-Qi Cao <wenqicao@gmail.com>
2014-06-22 14:18:47 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -5,50 +5,42 @@ using namespace std;
 template<class T>
 struct ListNode{
 	T data;
-	shared_ptr<ListNode<T>> next;
+	shared_ptr<ListNode<int>> next;
 };
-void append_and_continue(shared_ptr<ListNode<int>>& head,shared_ptr<ListNode<int>>& tail,shared_ptr<ListNode<int>>& node);
-void append_node(shared_ptr<ListNode<int>>& head,shared_ptr<ListNode<int>>&tail, shared_ptr<ListNode<int>>& node);
 
-shared_ptr<ListNode<int>> merge_list(shared_ptr<ListNode<int>> L, shared_ptr<ListNode<int>> R){
-	shared_ptr<ListNode<int>> head=nullptr, tail=nullptr;
-	while(L&&R){
-		append_and_continue(head,tail,L->data < R->data ?L:R);
+shared_ptr<ListNode<int>> reverse_sublist(shared_ptr<ListNode<int>> L, int start, int end){
+	if(start==end){
+		return L;
 	}
-	if(L)
-		append_node(head,tail,L);
-	if(R)
-		append_node(head,tail,R);
-	return head;
+	size_t count=1;
+	shared_ptr<ListNode<int>> start_ptr=L, pre_start_ptr=nullptr;
+	while(count<start){
+		count++;
+		pre_start_ptr = start_ptr;
+		start_ptr = start_ptr->next;
+	}
+	if(pre_start_ptr){
+		pre_start_ptr->next=nullptr;
+	}
+	shared_ptr<ListNode<int>> cur=start_ptr,pre=nullptr,end_ptr=nullptr,next_end_ptr=nullptr;
+	while(count<end){
+		count++;
+		end_ptr=cur->next;
+		next_end_ptr = end_ptr->next;
+		cur->next = pre;
+		pre = cur;
+		cur = end_ptr;
+	}
+	end_ptr->next = pre;
+	start_ptr->next=next_end_ptr;
+	if(pre_start_ptr){
+		pre_start_ptr->next=end_ptr;
+		return L;
+	}else{
+		return end_ptr;
+	}
+
 }
-
-void append_and_continue(shared_ptr<ListNode<int>>& head,shared_ptr<ListNode<int>>& tail,shared_ptr<ListNode<int>>& node){
-	append_node(head,tail,node);
-	node=node->next;
-}
-
-void append_node(shared_ptr<ListNode<int>>& head,shared_ptr<ListNode<int>>&tail, shared_ptr<ListNode<int>>& node){
-	head? tail->next=node : head=node;
-	tail=node;
-}
-
-
-
-
 int main(){
-	shared_ptr<ListNode<int>> L1 = make_shared<ListNode<int>>(ListNode<int>{1,nullptr});
-	shared_ptr<ListNode<int>> L2 = make_shared<ListNode<int>>(ListNode<int>{3,nullptr});
-	shared_ptr<ListNode<int>> L3 = make_shared<ListNode<int>>(ListNode<int>{5,nullptr});
-	L1->next=L2;
-	L2->next=L3;
-	shared_ptr<ListNode<int>> L4 = make_shared<ListNode<int>>(ListNode<int>{2,nullptr});
-	shared_ptr<ListNode<int>> L5 = make_shared<ListNode<int>>(ListNode<int>{4,nullptr});
-	shared_ptr<ListNode<int>> L6 = make_shared<ListNode<int>>(ListNode<int>{6,nullptr});
-	L4->next=L5;
-	L5->next=L6;
-	shared_ptr<ListNode<int>> result = merge_list(L1,L4);
-	while(result){
-		cout<<result->data<<" ";
-		result=result->next;
-	}
+	
 }
