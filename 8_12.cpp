708be396d281ce385176f4fc9c39d6d63a0f96bb
Wen-Qi Cao <wenqicao@gmail.com>
2014-06-23 14:17:27 -0400

    init

--- 8_12.cpp
+++ 8_12.cpp
@@ -45,4 +45,5 @@ int main(){
 		cout<<result->data;
 		result=result->next; 
 	}
+	
 }
--- proto.cpp
+++ proto.cpp
@@ -8,33 +8,28 @@ struct ListNode{
 	shared_ptr<ListNode<T>> next;
 };
 
-shared_ptr<ListNode<int>> remove_duplicate(shared_ptr<ListNode<int>>& head){
-	shared_ptr<ListNode<int>> cur=head, pre=nullptr;
-	while(cur){
-		if(pre&&pre->data==cur->data){
-			pre->next=cur->next;
-			cur=cur->next;
-		}else{
-			pre=cur;
-			cur=cur->next;
+shared_ptr<ListNode<int>> even_odd_order(shared_ptr<ListNode<int>>& head){
+	if(!head->next){
+		return nullptr;
+	}
+	shared_ptr<ListNode<int>> even_cur=head, pre_even_cur=nullptr, odd_cur=head->next,odd_first=head;
+	while(even_cur&&odd_cur){
+		even_cur->next=odd_cur->next;
+		pre_even_cur=even_cur;
+		even_cur=even_cur->next;
+		if(even_cur){
+			odd_cur->next=even_cur->next;
+			odd_cur=odd_cur->next;
 		}
 	}
+	if(even_cur){
+		pre_even_cur=even_cur;
+	}
+	if(pre_even_cur)
+		pre_even_cur->next=odd_first;
 	return head;
 }
 
 int main(){
-	shared_ptr<ListNode<int>> L1 = make_shared<ListNode<int>>(ListNode<int>{1,nullptr});
-	shared_ptr<ListNode<int>> L2 = make_shared<ListNode<int>>(ListNode<int>{2,nullptr});
-	shared_ptr<ListNode<int>> L3 = make_shared<ListNode<int>>(ListNode<int>{3,nullptr});
-	shared_ptr<ListNode<int>> L4 = make_shared<ListNode<int>>(ListNode<int>{3,nullptr});
-	shared_ptr<ListNode<int>> L5 = make_shared<ListNode<int>>(ListNode<int>{5,nullptr});
-	L1->next=L2;
-	L2->next=L3;
-	L3->next=L4;
-	L4->next=L5;
-	shared_ptr<ListNode<int>> result =  remove_duplicate(L1);
-	while(result){
-		cout<<result->data<<" ";
-		result=result->next;
-	}
+
 }
