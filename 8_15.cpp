436af827427bb4b9457914e1a75ccfc91403560a
Wen-Qi Cao <wenqicao@gmail.com>
2014-06-23 14:39:27 -0400

    init

---
+++ 8_15.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <memory>
+using namespace std;
+
+template<class T>
+struct ListNode{
+	T data;
+	shared_ptr<ListNode<int>> next;
+};
+
+shared_ptr<ListNode<int>> reverse_list(shared_ptr<ListNode<int>>& head){
+	shared_ptr<ListNode<int>> cur=head, pre=nullptr;
+	while(cur){
+		shared_ptr<ListNode<int>> temp = cur->next;
+		cur->next=pre;
+		pre=cur;
+		cur=temp;
+	}
+	return pre;
+}
+
+bool check_parlidrome(shared_ptr<ListNode<int>> head){
+	shared_ptr<ListNode<int>> slow=head, fast=head;
+	while(fast){
+		fast=fast->next;
+		if(fast){
+			fast=fast->next;
+			slow=slow->next;
+		}
+	}
+	shared_ptr<ListNode<int>> res = reverse_list(slow);
+	while(head && slow){
+		if(slow->data != head->data){
+			return false;
+		}
+		head=head->next;
+		slow=slow->next;
+	}
+	return true;
+}
+
+int main(){
+	shared_ptr<ListNode<int>> L1 = make_shared<ListNode<int>>(ListNode<int>{1,nullptr});
+	shared_ptr<ListNode<int>> L2 = make_shared<ListNode<int>>(ListNode<int>{2,nullptr});
+	shared_ptr<ListNode<int>> L3 = make_shared<ListNode<int>>(ListNode<int>{2,nullptr});
+	shared_ptr<ListNode<int>> L4 = make_shared<ListNode<int>>(ListNode<int>{1,nullptr});
+	shared_ptr<ListNode<int>> L5 = make_shared<ListNode<int>>(ListNode<int>{1,nullptr});
+	L1->next=L2;
+	L2->next=L3;
+	L3->next=L4;
+	// L4->next=L5;
+	L5->next=nullptr;
+	
+	cout<<check_parlidrome(L1);
+	// shared_ptr<ListNode<int>> result = reverse_list(L1);
+	// while(result){
+	// 	cout<<result->data<<" ";
+	// 	result=result->next;
+	// }
+}
+
