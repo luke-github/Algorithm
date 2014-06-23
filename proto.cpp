9620259639db476ccdd55952f4e421284f91d351
Wen-Qi Cao <wenqicao@gmail.com>
2014-06-23 18:19:52 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,55 +1,37 @@
 #include <iostream>
-#include <memory>
+#include <stack>
 using namespace std;
 
-template<class T>
-struct ListNode{
-	T data;
-	shared_ptr<ListNode<int>> next;
+class My_Stack{
+public:
+	void push(int x){
+		s_.emplace(x,s_.empty()? x : max(x,s_.top().second));
+	}
+	int pop(){
+		if(s_.empty()){
+			throw length_error("the stack is empty");
+		}
+		int result = s_.top().first;
+		s_.pop();
+		return result;
+	}
+	int max_num(){
+		if(s_.empty()){
+			throw length_error("the stack is empty");
+		}
+		return s_.top().second;
+	}
+private:
+	stack<pair<int,int>> s_;
 };
 
-bool handle_carry(shared_ptr<ListNode<int>>& head){
-	if(head->data<10)
-		return false;
-	head->data %= 10;
-	if(!head->next){
-		head->next = make_shared<ListNode<int>>(ListNode<int>{0,nullptr});
-	}
-	head->next->data++;
-	return true;
-}
-
-shared_ptr<ListNode<int>> add_two_list(shared_ptr<ListNode<int>>& l1,shared_ptr<ListNode<int>>& l2){
-	shared_ptr<ListNode<int>> result = l1;
-	while(l2){
-		l1->data+=l2->data;
-		handle_carry(l1);
-		if(!l1->next){
-			l1->next=l2->next;
-			return result;
-		}
-		l1=l1->next;
-		l2=l2->next;
-	}
-	while(l1&&handle_carry(l1)){
-		l1=l1->next;
-	}
-	return result;
-}
-
 int main(){
-	shared_ptr<ListNode<int>> L1 = make_shared<ListNode<int>>(ListNode<int>{1,nullptr});
-	shared_ptr<ListNode<int>> L2 = make_shared<ListNode<int>>(ListNode<int>{0,nullptr});
-	shared_ptr<ListNode<int>> L3 = make_shared<ListNode<int>>(ListNode<int>{9,nullptr});
-	shared_ptr<ListNode<int>> L4 = make_shared<ListNode<int>>(ListNode<int>{9,nullptr});
-	shared_ptr<ListNode<int>> L5 = make_shared<ListNode<int>>(ListNode<int>{9,nullptr});
-	L1->next=L2;
-	// L2->next=L3;
-	L3->next=L4;
-	L4->next=L5;
-	shared_ptr<ListNode<int>> result = add_two_list(L1,L3);
-	while(result){
-		cout<<result->data<<" ";
-		result=result->next;
-	}
+	My_Stack st;
+	st.push(1);
+	st.push(2);
+	st.push(3);
+	st.push(4);
+	cout<<st.max_num()<<endl;
+	st.pop();
+	cout<<st.max_num()<<endl;
 }
