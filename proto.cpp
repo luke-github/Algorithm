f78a3957cc154931675603060973436d8468d542
Wen-Qi Cao <wenqicao@gmail.com>
2014-06-24 18:35:46 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,37 +1,54 @@
 #include <iostream>
-#include <stack>
+#include <memory>
+#include <queue>
 using namespace std;
-void insert_node(int node,stack<int>* st);
-void sort_stack(stack<int>* st){
-	if(!st->empty()){
-		int temp = st->top();
-		st->pop();
-		sort_stack(st);
-		insert_node(temp,st);
-	}
-}
 
-void insert_node(int node,stack<int>* st){
-	if(st->empty()||node>=st->top()){
-		st->push(node);
-	}else{
-		int temp = st->top();
-		st->pop();
-		insert_node(node,st);
-		st->push(temp);
+template<class T>
+struct Node{
+	T data;
+	shared_ptr<Node<int>> left;
+	shared_ptr<Node<int>> right;
+};
+
+void print_binary_tree(shared_ptr<Node<int>>& head){
+	if(!head){
+		return;
+	}
+	queue<shared_ptr<Node<int>>> q;
+	q.emplace(head);
+	int count = q.size();
+	while(!q.empty()){
+		cout<<q.front()->data<<" ";
+		shared_ptr<Node<int>> temp = q.front();
+		if(temp->left){
+			q.emplace(temp->left);
+		}
+		if(temp->right){
+			q.emplace(temp->right);
+		}
+		q.pop();
+		if(--count==0){
+			count=q.size();
+			cout<<endl;
+		}
 	}
 }
 
 int main(){
-	stack<int> my_stack;
-	my_stack.push(1);
-	my_stack.push(4);
-	my_stack.push(2);
-	my_stack.push(7);
-	my_stack.push(5);
-	sort_stack(&my_stack);
-	while(!my_stack.empty()){
-		cout<<my_stack.top()<<" ";
-		my_stack.pop();
-	}
+	shared_ptr<Node<int>> L1 = make_shared<Node<int>>(Node<int>{1,nullptr,nullptr});
+	shared_ptr<Node<int>> L2 = make_shared<Node<int>>(Node<int>{2,nullptr,nullptr});
+	shared_ptr<Node<int>> L3 = make_shared<Node<int>>(Node<int>{3,nullptr,nullptr});
+	shared_ptr<Node<int>> L4 = make_shared<Node<int>>(Node<int>{4,nullptr,nullptr});
+	shared_ptr<Node<int>> L5 = make_shared<Node<int>>(Node<int>{5,nullptr,nullptr});
+	shared_ptr<Node<int>> L6 = make_shared<Node<int>>(Node<int>{6,nullptr,nullptr});
+	shared_ptr<Node<int>> L7 = make_shared<Node<int>>(Node<int>{7,nullptr,nullptr});
+	shared_ptr<Node<int>> L8 = make_shared<Node<int>>(Node<int>{8,nullptr,nullptr});
+	L1->left=L2;
+	L1->right=L3;
+	L2->left=L4;
+	L2->right=L5;
+	L3->left=L6;
+	L3->right=L7;
+	L4->left=L8;
+	print_binary_tree(L1);
 }
