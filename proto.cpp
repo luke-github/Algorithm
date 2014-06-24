f26bc7ceb785ff73ea2a08bf3e698ec95b8a10c6
Wen-Qi Cao <wenqicao@gmail.com>
2014-06-24 17:30:07 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,59 +1,25 @@
 #include <iostream>
-#include <stack>
-#include <memory>
+#include <vector>
+#include <sstream>
 using namespace std;
 
-template<class T>
-struct ListNode{
-	T data;
-	T order;
-	shared_ptr<ListNode<int>> jump;
-	shared_ptr<ListNode<int>> next;
-};
-
-void posting_list_order_recursion(shared_ptr<ListNode<int>>& node, int& counter){
-	if(node&&node->order==-1){
-		node->order=counter++;
-		posting_list_order_recursion(node->jump,counter);
-		posting_list_order_recursion(node->next,counter);
-	}
-}
-
-void posting_list_order_iteration(shared_ptr<ListNode<int>>& node){
-	int counter=0;
-	stack<shared_ptr<ListNode<int>>> stk;
-	stk.emplace(node);
-	while(!stk.empty()){
-		shared_ptr<ListNode<int>> cur = stk.top();
-		stk.pop();
-		if(cur&&cur->order==-1){
-			cur->order=counter++;
-			stk.emplace(cur->next);
-			stk.emplace(cur->jump);
+vector<pair<int,int>> sunset_solution(istringstream* sin){
+	vector<pair<int,int>> vec;
+	int height;
+	int index=0;
+	while(*sin>>height){
+		while(!vec.empty()&&height>=vec.back().second){
+			vec.pop_back();
 		}
+		vec.emplace_back(index++,height);
 	}
+	return vec;
 }
 
 int main(){
-	shared_ptr<ListNode<int>> L1 = make_shared<ListNode<int>>(ListNode<int>{1,-1,nullptr,nullptr});
-	shared_ptr<ListNode<int>> L2 = make_shared<ListNode<int>>(ListNode<int>{2,-1,nullptr,nullptr});
-	shared_ptr<ListNode<int>> L3 = make_shared<ListNode<int>>(ListNode<int>{3,-1,nullptr,nullptr});
-	shared_ptr<ListNode<int>> L4 = make_shared<ListNode<int>>(ListNode<int>{4,-1,nullptr,nullptr});
-	shared_ptr<ListNode<int>> L5 = make_shared<ListNode<int>>(ListNode<int>{5,-1,nullptr,nullptr});
-	L1->next=L2;
-	L2->next=L3;
-	L3->next=L4;
-	L4->next=L5;
-	L1->jump=L3;
-	L3->jump=L5;
-	L2->jump=L4;
-	L4->jump=L5;
-	L5->jump=L1;
-	int x=0;
-	// posting_list_order_recursion(L1,x);
-	posting_list_order_iteration(L1);
-	while(L1){
-		cout<<L1->order<<" ";
-		L1=L1->next;
-	}
+	string input = " 1 2 3 4 5 6 5 4 3";
+	istringstream ss(input);
+	vector<pair<int,int>> result = sunset_solution(&ss);
+	for(int i=0;i<result.size();i++)
+		cout<<result[i].first<<" "<<result[i].second<<endl;
 }
