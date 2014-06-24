730d7f865c9873e649d99fd069e5136b7ea83d5b
Wen-Qi Cao <wenqicao@gmail.com>
2014-06-24 13:57:38 -0400

    init

---
+++ 9_3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool check_parenthese(const string& s){
+	stack<char> stk;
+	for(int i=0;i<s.size();i++){
+		if(s[i]=='('||s[i]=='['||s[i]=='{'){
+			stk.emplace(s[i]);
+		}else{
+			if(stk.empty()){
+				return false;
+			}
+			if((s[i]=='('&&stk.top()!=')') ||
+				(s[i]=='['&&stk.top()!=']') ||
+			(s[i]=='{'&&stk.top()!='}')) {
+				return false;
+			}
+			stk.pop();
+		}
+	}
+	return stk.empty();
+}
+
+int main(){
+	string input = "()[]{}{[()()]}(";
+	cout<<check_parenthese(input);
+}
