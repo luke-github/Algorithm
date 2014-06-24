1ff3dd200d1862fdf73490eaf7fd8f1aa3af6b6e
Wen-Qi Cao <wenqicao@gmail.com>
2014-06-24 14:51:29 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,27 +1,47 @@
 #include <iostream>
-#include <stack>
+#include <vector>
+#include <sstream>
 using namespace std;
 
-
-bool check_parenthese(const string& s){
-	stack<char> stk;
-	for(int i=0;i<s.size();i++){
-		if(s[i]=='('||s[i]=='['||s[i]=='{'){
-			stk.emplace(s[i]);
-		}else{
-			if(stk.empty())
-				return false;
-			if((s[i]==')'&&stk.top()!='(') || 
-				(s[i]==']'&&stk.top()!='[') || 
-			(s[i]=='}'&&stk.top()!='{'))
-				return false;
-			stk.pop();
+string shortest_filepath(const string& path){
+	vector<string> stk;
+	stringstream ss(path);
+	string token;
+	if(path.front()=='/'){
+		stk.push_back("/");
+	}
+	while(getline(ss,token,'/')){
+		if(token==".."){
+			if(stk.empty()||stk.back()==".."){
+				stk.push_back("..");
+			}else{
+				if(stk.back()=="/"){
+					throw invalid_argument("invalid_input");
+				}
+				stk.pop_back();
+			}
+		}else if(token!=""&&token!="."){
+			for(const char c : token){
+				if(!isalnum(c)){
+					throw invalid_argument("this is not a valid input");
+				}
+			}
+			stk.push_back(token);
 		}
 	}
-	return stk.empty();
+	string result;
+	result = stk[0];
+	for(int i=1;i<stk.size();i++){
+		if(stk[i-1]!="/")
+			result += "/";
+		result += stk[i];
+	}
+	return result;
 }
 
 int main(){
-	string input = "()[]{}{[()()]}";
-	cout<<check_parenthese(input);
+	string input = "/a1/b2/c3/./../../file";
+	cout<<shortest_filepath(input);
 }
+
+
