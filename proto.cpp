8ee5134be36a48208fbf7519dee12d4a9d017980
Wen-Qi Cao <wenqicao@gmail.com>
2014-06-24 15:18:14 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,47 +1,39 @@
 #include <iostream>
-#include <vector>
-#include <sstream>
+#include <stack>
+#include <memory>
 using namespace std;
 
-string shortest_filepath(const string& path){
-	vector<string> stk;
-	stringstream ss(path);
-	string token;
-	if(path.front()=='/'){
-		stk.push_back("/");
-	}
-	while(getline(ss,token,'/')){
-		if(token==".."){
-			if(stk.empty()||stk.back()==".."){
-				stk.push_back("..");
-			}else{
-				if(stk.back()=="/"){
-					throw invalid_argument("invalid_input");
-				}
-				stk.pop_back();
-			}
-		}else if(token!=""&&token!="."){
-			for(const char c : token){
-				if(!isalnum(c)){
-					throw invalid_argument("this is not a valid input");
-				}
-			}
-			stk.push_back(token);
+template<class T>
+struct BST_Node{
+	T data;
+	shared_ptr<BST_Node<int>> left;
+	shared_ptr<BST_Node<int>> right;
+};
+
+void print_BST_nodes(shared_ptr<BST_Node<int>>& node){
+	stack<shared_ptr<BST_Node<int>>> stk;
+	while(!stk.empty()||node){
+		if(node){
+			stk.push(node);
+			node=node->left;
+		}else{
+			node=stk.top();
+			stk.pop();
+			cout<<node->data;
+			node=node->right;
 		}
 	}
-	string result;
-	result = stk[0];
-	for(int i=1;i<stk.size();i++){
-		if(stk[i-1]!="/")
-			result += "/";
-		result += stk[i];
-	}
-	return result;
 }
 
 int main(){
-	string input = "/a1/b2/c3/./../../file";
-	cout<<shortest_filepath(input);
+		shared_ptr<BST_Node<int>> L1 = make_shared<BST_Node<int>>(BST_Node<int>{1,nullptr});
+	shared_ptr<BST_Node<int>> L2 = make_shared<BST_Node<int>>(BST_Node<int>{2,nullptr});
+	shared_ptr<BST_Node<int>> L3 = make_shared<BST_Node<int>>(BST_Node<int>{3,nullptr});
+	shared_ptr<BST_Node<int>> L4 = make_shared<BST_Node<int>>(BST_Node<int>{4,nullptr});
+	shared_ptr<BST_Node<int>> L5 = make_shared<BST_Node<int>>(BST_Node<int>{5,nullptr});
+	L1->left=L2;
+	L1->right=L3;
+	L2->left=L4;
+	L3->right=L5;
+	print_BST_nodes(L1);
 }
-
-
