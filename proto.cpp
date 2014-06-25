de88e46596e9e810a184fc3df48dcab16cdb5064
Wen-Qi Cao <wenqicao@gmail.com>
2014-06-25 14:13:32 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,54 +1,56 @@
 #include <iostream>
-#include <memory>
-#include <queue>
+#include <vector>
 using namespace std;
 
-template<class T>
-struct Node{
-	T data;
-	shared_ptr<Node<int>> left;
-	shared_ptr<Node<int>> right;
+class My_Queue{
+public:
+	My_Queue(int capacity) : data_(capacity) {}
+
+	int size(){
+		return count_;
+	}
+
+	void enqueue(int x){
+		if(count_==data_.size()){
+			rotate(data_.begin(),data_.begin()+head_,data_.end());
+			head_=0;
+			tail_=count_;
+			data_.resize(data_.size()<<1);
+		}
+		data_[tail_]=x;
+		tail_=(tail_+1)%data_.size();
+		count_++;
+	}
+
+	int dequeue(){
+		if(!count_){
+			throw invalid_argument("the queue is empty");
+		}
+		int result = data_[head_];
+		count_--;
+		head_=(head_+1)%data_.size();
+		return result;
+
+	}
+private:
+	int head_=0,tail_=0,count_=0;
+	vector<int> data_;
 };
 
-void print_binary_tree(shared_ptr<Node<int>>& head){
-	if(!head){
-		return;
-	}
-	queue<shared_ptr<Node<int>>> q;
-	q.emplace(head);
-	int count = q.size();
-	while(!q.empty()){
-		cout<<q.front()->data<<" ";
-		shared_ptr<Node<int>> temp = q.front();
-		if(temp->left){
-			q.emplace(temp->left);
-		}
-		if(temp->right){
-			q.emplace(temp->right);
-		}
-		q.pop();
-		if(--count==0){
-			count=q.size();
-			cout<<endl;
-		}
-	}
-}
-
 int main(){
-	shared_ptr<Node<int>> L1 = make_shared<Node<int>>(Node<int>{1,nullptr,nullptr});
-	shared_ptr<Node<int>> L2 = make_shared<Node<int>>(Node<int>{2,nullptr,nullptr});
-	shared_ptr<Node<int>> L3 = make_shared<Node<int>>(Node<int>{3,nullptr,nullptr});
-	shared_ptr<Node<int>> L4 = make_shared<Node<int>>(Node<int>{4,nullptr,nullptr});
-	shared_ptr<Node<int>> L5 = make_shared<Node<int>>(Node<int>{5,nullptr,nullptr});
-	shared_ptr<Node<int>> L6 = make_shared<Node<int>>(Node<int>{6,nullptr,nullptr});
-	shared_ptr<Node<int>> L7 = make_shared<Node<int>>(Node<int>{7,nullptr,nullptr});
-	shared_ptr<Node<int>> L8 = make_shared<Node<int>>(Node<int>{8,nullptr,nullptr});
-	L1->left=L2;
-	L1->right=L3;
-	L2->left=L4;
-	L2->right=L5;
-	L3->left=L6;
-	L3->right=L7;
-	L4->left=L8;
-	print_binary_tree(L1);
+		My_Queue q(5);
+	q.enqueue(1);
+	q.enqueue(2);
+	q.enqueue(3);
+	q.enqueue(4);
+	q.enqueue(5);
+	q.enqueue(6);
+	cout<<q.dequeue()<<endl;
+	cout<<q.dequeue()<<endl;
+	cout<<q.dequeue()<<endl;	
+	cout<<q.dequeue()<<endl;
+	cout<<q.dequeue()<<endl;
+	cout<<q.dequeue()<<endl;
+	cout<<q.dequeue()<<endl;
+    cout<<q.size()<<endl;
 }
