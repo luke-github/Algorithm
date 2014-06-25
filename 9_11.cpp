b70ea2fd5d307fc477809f74368f6b7082decb4d
Wen-Qi Cao <wenqicao@gmail.com>
2014-06-25 14:05:39 -0400

    init

---
+++ 9_11.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class My_Queue{
+public:
+	My_Queue(int capacity) : data_(capacity) {}
+
+	int size(){ return count_;}
+
+	void enqueue(int x){
+		if(count_==data_.size()){
+			rotate(data_.begin(),data_.begin()+head_,data_.end());
+			data_.resize(data_.size()<<1);
+			head_=0;
+			tail_=count_;
+		}
+		data_[tail_]=x;
+		tail_=(tail_+1)%data_.size();
+		count_++;
+	}
+
+	int dequeue(){
+		if(!count_){
+			throw invalid_argument("the queue is empty");
+		}
+		int result = data_[head_];
+		head_ = (head_+1)%data_.size();
+		count_--;
+		return result;
+	}
+private:
+	int head_=0,tail_=0,count_=0;
+	vector<int> data_;
+};
+
+int main(){
+	My_Queue q(5);
+	q.enqueue(1);
+	q.enqueue(2);
+	q.enqueue(3);
+	q.enqueue(4);
+	q.enqueue(5);
+	q.enqueue(6);
+	cout<<q.dequeue()<<endl;
+	cout<<q.dequeue()<<endl;
+	cout<<q.dequeue()<<endl;	
+	cout<<q.dequeue()<<endl;
+	cout<<q.dequeue()<<endl;
+	cout<<q.dequeue()<<endl;
+	cout<<q.dequeue()<<endl;
+    cout<<q.size()<<endl;
+}
+
+
+
+
