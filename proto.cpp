a0e7d7396b3cd06743a6883e7bb76ef729ca9c75
Wen-Qi Cao <wenqicao@gmail.com>
2014-06-25 16:37:14 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -8,24 +8,21 @@ struct BST{
 	shared_ptr<BST<T>> left;
 	shared_ptr<BST<T>> right;
 };
-
-pair<shared_ptr<BST<int>>,int> k_unbalanced_node(shared_ptr<BST<int>>& head, int k){
-	if(!head){
-		return {nullptr,0};
-	}
-	auto left_result = k_unbalanced_node(head->left,k);
-	if(left_result.first)
-		return left_result;
-	auto right_result = k_unbalanced_node(head->right,k);
-	if(right_result.first)
-		return right_result;
-	int num = left_result.second + right_result.second + 1;
-	if(abs(left_result.second - right_result.second)>k){
-		return {head,num};
-	}
-	return {nullptr,num};
+bool check_symmetric_tree_handler(shared_ptr<BST<int>>& leftnode, shared_ptr<BST<int>>& rightnode);
+bool check_symmetric_tree(shared_ptr<BST<int>>& head){
+	return !head || check_symmetric_tree_handler(head->left,head->right);
 }
-
+bool check_symmetric_tree_handler(shared_ptr<BST<int>>& leftnode, shared_ptr<BST<int>>& rightnode){
+	if(!leftnode&&!rightnode)
+		return true;
+	else if(leftnode&&rightnode){
+		return leftnode->data==rightnode->data &&
+				check_symmetric_tree_handler(leftnode->left,rightnode->right) &&
+				check_symmetric_tree_handler(leftnode->right,rightnode->left);
+	}
+	else
+		return false;
+}
 int main(){
 	
 }
