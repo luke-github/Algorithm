630b7d9f0fcc1beafb25e04dcf8cb560b1a08068
Wen-Qi Cao <wenqicao@gmail.com>
2014-06-26 18:25:57 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <memory>
 #include <vector>
-#include <unordered_map>
+#include <stack>
 using namespace std;
 
 template<class T>
@@ -10,25 +10,44 @@ struct BST_Node{
 	shared_ptr<BST_Node<T>> left;
 	shared_ptr<BST_Node<T>> right;
 };
-shared_ptr<BST_Node<int>> construct_inorder_BST_handler(vector<int>& pre, int pre_s, int pre_e, vector<int>& in, int in_s, int in_e, unordered_map<int, size_t> in_entry_index_map);
-shared_ptr<BST_Node<int>> construct_inorder_BST(vector<int>& preorder,vector<int>& inorder){
-	unordered_map<int,size_t> in_entry_index_map;
-	for(int i=0;i<inorder.size();i++)
-		in_entry_index_map.emplace(inorder[i],i);
-	return construct_inorder_BST_handler(preorder,0,preorder.size(),inorder,0,inorder.size(),in_entry_index_map);
+
+shared_ptr<BST_Node<int>> reconstruct_BST(vector<int>& preorder){
+	stack<shared_ptr<BST_Node<int>>> st;
+	reverse(preorder.begin(),preorder.end());
+	for(auto it = preorder.begin();it!=preorder.end();it++){
+		if(!*it){
+			st.emplace(nullptr);
+		}else{
+			shared_ptr<BST_Node<int>> left = st.top();
+			st.pop();
+			shared_ptr<BST_Node<int>> right = st.top();
+			st.pop();
+			st.emplace(make_shared<BST_Node<int>>(BST_Node<int>{*it,left,right}));
+		}
+	}
+	return st.top();
 }
 
-shared_ptr<BST_Node<int>> construct_inorder_BST_handler(vector<int>& pre, int pre_s, int pre_e, vector<int>& in, int in_s, int in_e, unordered_map<int, size_t> in_entry_index_map){
-	if(pre_e>pre_s&&in_e>in_s){
-		int index = in_entry_index_map.at(pre[pre_s]);
-		int left_part = index - in_s;
-		return make_shared<BST_Node<int>>(BST_Node<int>({pre[pre_s],
-			construct_inorder_BST_handler(pre,pre_s+1,pre_s+1+left_part,in,in_s,index,in_entry_index_map),
-			construct_inorder_BST_handler(pre,pre_s+1+left_part,pre_e,in,index+1,in_e,in_entry_index_map)}));
+void print_result(shared_ptr<BST_Node<int>>& head){
+	if(!head){
+		return;
 	}
-	return nullptr;
+	if(head->left){
+		print_result(head->left);
+	}
+	cout<<head->data<<" ";
+	if(head->right){
+		print_result(head->right);
+	}
 }
 
 int main(){
-
+	vector<int> input = {1,2,4,0,0,5,0,0,3,0,0};
+	shared_ptr<BST_Node<int>> head = reconstruct_BST(input);
+	print_result(head);
 }
+//    L1
+//   /  \
+//  L2  L3
+//  / \
+// L4 L5
