d3b07c2974e9b64a23f95bb478620149bab951e8
Wen-Qi Cao <wenqicao@gmail.com>
2014-06-26 14:23:43 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -9,26 +9,13 @@ struct BST_Node{
 	shared_ptr<BST_Node<T>> right;
 };
 
-int calculate_sum(shared_ptr<BST_Node<int>>& head, int num){
-	if(!head)
-		return 0;
-	num = (num<<1) + head->data;
-	if(!head->left&&!head->right)
-		return num;
-	return calculate_sum(head->left,num)+calculate_sum(head->right,num);
-}
-
-int main(){
-		shared_ptr<BST_Node<int>> L1 = make_shared<BST_Node<int>>(BST_Node<int>{1,nullptr,nullptr});
-	shared_ptr<BST_Node<int>> L2 = make_shared<BST_Node<int>>(BST_Node<int>{0,nullptr,nullptr});
-	shared_ptr<BST_Node<int>> L3 = make_shared<BST_Node<int>>(BST_Node<int>{0,nullptr,nullptr});
-	shared_ptr<BST_Node<int>> L4 = make_shared<BST_Node<int>>(BST_Node<int>{1,nullptr,nullptr});
-	shared_ptr<BST_Node<int>> L5 = make_shared<BST_Node<int>>(BST_Node<int>{0,nullptr,nullptr});
-	shared_ptr<BST_Node<int>> L6 = make_shared<BST_Node<int>>(BST_Node<int>{1,nullptr,nullptr});
-	L1->left=L2;
-	L1->right=L3;
-	L2->left=L4;
-	L2->right=L5;
-	L3->left=L6;
-	cout<<calculate_sum(L1,0);
+bool check_path_sum(shared_ptr<BST_Node<int>>& head, int num, int sum){
+	if(!head){
+		return false;
+	}
+	num = num + head->data;
+	if(!head->left&&!head->right){
+		return num==sum;
+	}
+	return check_path_sum(head->left,num,sum) || check_path_sum(head->right,num,sum);
 }
