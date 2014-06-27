c865b5fb32344fee34a325d15ef183e124bcee67
Wen-Qi Cao <wenqicao@gmail.com>
2014-06-27 15:28:59 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,53 +1,33 @@
 #include <iostream>
 #include <memory>
-#include <vector>
-#include <stack>
+#include <list>
 using namespace std;
 
 template<class T>
-struct BST_Node{
+struct BSF_Node{
 	T data;
-	shared_ptr<BST_Node<T>> left;
-	shared_ptr<BST_Node<T>> right;
+	shared_ptr<BSF_Node<T>> left;
+	shared_ptr<BSF_Node<T>> right;
 };
-
-shared_ptr<BST_Node<int>> reconstruct_BST(vector<int>& preorder){
-	stack<shared_ptr<BST_Node<int>>> st;
-	reverse(preorder.begin(),preorder.end());
-	for(auto it = preorder.begin();it!=preorder.end();it++){
-		if(!*it){
-			st.emplace(nullptr);
-		}else{
-			shared_ptr<BST_Node<int>> left = st.top();
-			st.pop();
-			shared_ptr<BST_Node<int>> right = st.top();
-			st.pop();
-			st.emplace(make_shared<BST_Node<int>>(BST_Node<int>{*it,left,right}));
-		}
-	}
-	return st.top();
+void generate_leave_node_list_handler(shared_ptr<BSF_Node<int>>& node, list<shared_ptr<BSF_Node<int>>>* result);
+list<shared_ptr<BSF_Node<int>>> generate_leave_node_list(shared_ptr<BSF_Node<int>>& head){
+	list<shared_ptr<BSF_Node<int>>> return_list;
+	generate_leave_node_list_handler(head,&return_list);
+	return return_list;
 }
 
-void print_result(shared_ptr<BST_Node<int>>& head){
-	if(!head){
-		return;
-	}
-	if(head->left){
-		print_result(head->left);
-	}
-	cout<<head->data<<" ";
-	if(head->right){
-		print_result(head->right);
+void generate_leave_node_list_handler(shared_ptr<BSF_Node<int>>& node, list<shared_ptr<BSF_Node<int>>>* result){
+	if(node){
+		if(!node->left&&!node->right){
+			result->emplace_back(node);
+		}else{
+			generate_leave_node_list_handler(node->left,result);
+			generate_leave_node_list_handler(node->right,result);
+		}
 	}
 }
 
 int main(){
-	vector<int> input = {1,2,4,0,0,5,0,0,3,0,0};
-	shared_ptr<BST_Node<int>> head = reconstruct_BST(input);
-	print_result(head);
+	
 }
-//    L1
-//   /  \
-//  L2  L3
-//  / \
-// L4 L5
+
