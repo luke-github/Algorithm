190eb5222b9a209c11c6b759ad13f8d342b55e45
Wen-Qi Cao <wenqicao@gmail.com>
2014-06-28 15:35:34 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,45 +1,38 @@
 #include <iostream>
-#include <memory>
+#include <queue>
 using namespace std;
 
-class BST_Node{
-public:
-	bool isLocked(){
-		return lock_;
+struct cmp{
+	bool operator()(const pair<int,int>& l, const pair<int,int>& r){
+		return l.first>r.first;
 	}
-	void lock(){
-		if(!lock_&&numoflocks_==0){
-			shared_ptr<BST_Node> ptr = parent_;
-			while(ptr){
-				if(ptr->lock_){
-					return;
-				}
-				ptr=ptr->parent_;
-			}
-			lock_=true;
-			ptr=parent_;
-			while(ptr){
-				ptr->numoflocks_++;
-				ptr=ptr->parent_;
-			}
-		}
-	}
-	void unLock(){
-		if(lock_){
-			shared_ptr<BST_Node> ptr = parent_;
-			while(ptr){
-				ptr->numoflocks_--;
-				ptr=ptr->parent_;
-			}
-			lock_=false;
-		}
-	}
-
-private:
-	shared_ptr<BST_Node> left_,right_,parent_;
-	bool lock_;
-	int numoflocks_;
 };
+
+vector<int> merge_sort(vector<vector<int>>& vec){
+	priority_queue<pair<int,int>,vector<pair<int,int>>,cmp> mini_heap;
+	vector<int> result;
+	vector<int> index(vec.size(),0);
+	for(int i=0;i<vec.size();i++){
+		if(vec[i].size()>0){
+			mini_heap.emplace(vec[i][0],i);
+			index[i]=1;
+		}
+	}
+	while(!mini_heap.empty()){
+		pair<int,int> mini_result = mini_heap.top();
+		result.emplace_back(mini_result.first);
+		if(index[mini_result.second]<vec[mini_result.second].size()){
+			mini_heap.emplace(vec[mini_result.second][index[mini_result.second]++],mini_result.second);
+		}
+		mini_heap.pop();
+	}
+	return result;
+}
+
 int main(){
-	
+		vector<vector<int>> vec={{1,10,100},{2,20,200},{3,30,300}};
+	vector<int> res = merge_sort(vec);
+	for(int x : res){
+		cout<<x<<" ";
+	}
 }
