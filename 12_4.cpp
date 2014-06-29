1bd042c8dc7b2cbe235dd47700567d3b52e54f7f
Wen-Qi Cao <wenqicao@gmail.com>
2014-06-29 16:21:50 -0400

    init

---
+++ 12_4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int smallest_search(vector<int>& vec){
+	int l=0,r=vec.size()-1;
+	while(l<r){
+		int m = (l+r)>>1;
+		if(vec[m]>vec[r]){
+			l=m+1;
+		}else{
+			r=m;
+		}
+	}
+	return vec[l];
+}
+int main(){
+	vector<int> input = {4,5,6,7,8,9,10,1,2,3};
+	cout<<smallest_search(input);
+}
