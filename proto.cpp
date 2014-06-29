48da6a0e89f194eed8a421c247ddb1c4c4ead647
Wen-Qi Cao <wenqicao@gmail.com>
2014-06-29 15:37:52 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -2,23 +2,21 @@
 #include <vector>
 using namespace std;
 
-int enhance_binary_search(vector<int>& vec, int k){
+int larger_than(vector<int>& vec, int k){
 	int l=0,r=vec.size()-1,res=-1;
 	while(l<=r){
-		int m = (l+r)/2;
-		if(vec[m]==k){
+		int m = (l+r)>>1;
+		if(vec[m]>k){
 			res=m;
 			r=m-1;
-		}else if(k>vec[m]){
-			l=m+1;
 		}else{
-			r=m-1;
+			l=m+1;
 		}
 	}
 	return res;
 }
 
 int main(){
-	vector<int> input = {1,2,3,6,6,6,6,6,7,8};
-	cout<<enhance_binary_search(input,6);
+	vector<int> input = {1,3,5,7,9};
+	cout<<larger_than(input,4);
 }
