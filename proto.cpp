bc54a1f62169002fac02266e116eb7c99d99970f
Wen-Qi Cao <wenqicao@gmail.com>
2014-06-29 15:56:53 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -2,21 +2,18 @@
 #include <vector>
 using namespace std;
 
-int larger_than(vector<int>& vec, int k){
+int search_value_index_equal(vector<int>& vec){
 	int l=0,r=vec.size()-1,res=-1;
 	while(l<=r){
 		int m = (l+r)>>1;
-		if(vec[m]>k){
-			res=m;
+		int val = vec[m]-m;
+		if(val==0){
+			return m;
+		}else if(val>0){
 			r=m-1;
 		}else{
 			l=m+1;
 		}
 	}
 	return res;
-}
-
-int main(){
-	vector<int> input = {1,3,5,7,9};
-	cout<<larger_than(input,4);
 }
