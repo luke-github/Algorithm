31674ddcf3abc28966cfa8539e19a298ef67952f
Wen-Qi Cao <wenqicao@gmail.com>
2014-07-01 18:57:10 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
+#include <unordered_map>
 #include <vector>
 using namespace std;
 
-pair<int,int> missing_and_duplicating(vector<int>& vec){
-	int sum=0,square_sum=0;
-	for(int i=0;i<vec.size();i++){
-		sum+=i-vec[i];
-		square_sum+=i*i-vec[i]*vec[i];
+void dispatch_anagram(const vector<string>& dict){
+	unordered_map<string,vector<string>> hash;
+	for(const string& s : dict){
+		string sorted_str(s);
+		sort(sorted_str.begin(),sorted_str.end());
+		hash[sorted_str].emplace_back(s);
 	}
-	return {(square_sum/sum+sum)/2,(square_sum/sum-sum)/2};
+	for(const pair<string,vector<string>>& p : hash){
+		if(p.second.size()>1){
+			for(const string& x : p.second){
+				cout<<x<<":";
+			}
+			cout<<endl;
+		}
+	}
 }
+
 int main(){
-	vector<int> input = {0,1,2,3,3,5,6,7,8};
-	pair<int,int> res = missing_and_duplicating(input);
-	cout<<res.first<<" "<<res.second;
+	vector<string> input = {"abc","acb","bac","qwe","ewq","wqe","wsx","wxs"};
+	dispatch_anagram(input);
 }
