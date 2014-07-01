62e4e462f6bb6160994cf4444e13421789c36f50
Wen-Qi Cao <wenqicao@gmail.com>
2014-07-01 15:26:56 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,27 +1,41 @@
 #include <iostream>
 #include <vector>
+#include <random>
 using namespace std;
 
-pair<int,int> find_min_max(vector<int>& vec){
-	if(vec.size()<=1){
-		return {vec.front(),vec.front()};
+int partition(int left, int right, int pivot, vector<int>* v);
+
+int kth_greatest_value(vector<int>& vec, int k){
+	int left=0,right=vec.size()-1;
+	while(left<=right){
+		default_random_engine gen((random_device())());
+		uniform_int_distribution<int> dis(left,right);
+		int position = partition(left,right,dis(gen),&vec);
+		if(position==k-1){
+			return vec[position];
+		}else if(position>k-1){
+			right = position -1;
+		}else{
+			left = position+1;
+		}
 	}
-	pair<int,int> result = minmax(vec[0],vec[1]);
-	for(int i=2;i<vec.size()-1;i++){	
-		pair<int,int> local = minmax(vec[i],vec[i+1]);
-		result = {min(result.first,local.first),
-					max(result.second,local.second)};
-	}
-	if(vec.size()&1){
-		result = {min(result.first,vec.back()),
-			max(result.second, vec.back())};
-	}
-	return result;
+	throw invalid_argument("no result");
 }
 
+int partition(int left, int right, int pivot, vector<int>* v){
+	int pivot_value = (*v)[pivot];
+	swap((*v)[pivot],(*v)[right]);
+	int larger_index = left;
+	for(int i=left;i<right;i++){
+		if((*v)[i]>pivot_value){
+			swap((*v)[i],(*v)[larger_index++]);
+		}
+	}
+	swap((*v)[right],(*v)[larger_index]);
+	return larger_index;
+}
 
 int main(){
-	vector<int> input = {1,2,3,4,5,6,7,8,9,0};
-	pair<int,int> res = find_min_max(input);
-	cout<<res.first<<" "<<res.second;
+	vector<int> input = {9,8,7,6,5,4,3,2,1};
+	cout<<kth_greatest_value(input,1);
 }
