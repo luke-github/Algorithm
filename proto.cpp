b7fc2000ed16e597db0336cf643df95c0dcbabf5
Wen-Qi Cao <wenqicao@gmail.com>
2014-07-01 18:34:37 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-int main(){
-	bitset<8> bit_set;
-	bit_set.set(1);
-	bit_set.set(2);
-	bit_set.set(3);
-	cout<<bit_set.test(4);
 
+pair<int,int> missing_and_duplicating(vector<int>& vec){
+	int sum=0,square_sum=0;
+	for(int i=0;i<vec.size();i++){
+		sum+=i-vec[i];
+		square_sum+=i*i-vec[i]*vec[i];
+	}
+	return {(square_sum/sum+sum)/2,(square_sum/sum-sum)/2};
+}
+int main(){
+	vector<int> input = {0,1,2,3,3,5,6,7,8};
+	pair<int,int> res = missing_and_duplicating(input);
+	cout<<res.first<<" "<<res.second;
 }
