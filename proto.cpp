36e322b44a7c4874b2cb18f93a900841956075de
Wen-Qi Cao <wenqicao@gmail.com>
2014-07-02 16:22:51 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,59 +1,34 @@
 #include <iostream>
-#include <unordered_map>
-#include <list>
+#include <memory>
+#include <unordered_set>
 using namespace std;
 
-template<size_t capacity>
-class LRU{
-public:
-	bool lookup(int isbn,int* price){
-		auto it = cache_.find(isbn);
-		if(it==cache_.end())
-			return false;
-		*price = it->second.second;
-		moveToFront(isbn,it);
-		return true;
-	}
-	void insert(int isbn,int price){
-		auto it = cache_.find(isbn);
-		if(it!=cache_.end()){
-			moveToFront(isbn,it);
-		}else{
-			if(cache_.size()==capacity){
-				cache_.erase(data_.back());
-				data_.pop_back();
-			}
-			data_.emplace_front(isbn);
-			cache_[isbn]={data_.begin(),price};
-		}
-	}
-	bool erase(int isbn){
-		auto it = cache_.find(isbn);
-		if(it==cache_.end()){
-			return false;
-		}
-		data_.erase(it->second.first);
-		cache_.erase(it);
-		return true;
-	}
-private:
-	typedef unordered_map<int,pair<list<int>::iterator,int>> Table;
-	Table cache_;
-	list<int> data_;
-	void moveToFront(int isbn, Table::iterator& it){
-		data_.erase(it->second.first);
-		data_.emplace_front(isbn);
-		it->second.first=data_.begin();
-	}
+template<class T>
+struct ListNode{
+	T data;
+	shared_ptr<ListNode<T>> left;
+	shared_ptr<ListNode<T>> right;
+	shared_ptr<ListNode<T>> parent;
 };
 
+shared_ptr<ListNode<int>> LCA(shared_ptr<ListNode<int>>& a, shared_ptr<ListNode<int>>& b){
+	unordered_set<shared_ptr<ListNode<int>>> hash;
+	while(a||b){
+		if(a){
+			if(hash.emplace(a).second==false){
+				return a;
+			}
+			a=a->parent;
+		}
+		if(b){
+			if(hash.emplace(b).second==false){
+				return b;
+			}
+			b=b->parent;
+		}
+	}
+	throw invalid_argument("invalid");
+}
 int main(){
-	LRU<5> t_lru;
-	t_lru.insert(1,10);
-	t_lru.insert(2,20);
-	t_lru.insert(3,30);
-	t_lru.insert(4,40);
-	t_lru.insert(5,50);
-	t_lru.insert(6,60);
-
+	
 }
