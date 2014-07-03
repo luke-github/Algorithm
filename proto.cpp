fbf9a820fe0295d3f30579803ad38dcf4fad0e00
Wen-Qi Cao <wenqicao@gmail.com>
2014-07-02 21:05:22 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,36 +1,45 @@
 #include <iostream>
-#include <unordered_map>
-#include <set>
 #include <vector>
-#include <fstream>
+#include <unordered_map>
 using namespace std;
 
-pair<string,string> affinity_pair(ifstream* ifs){
-	unordered_map<string,set<string>> hash;
-	string page,user;
-	pair<string,string> res;
-	int max_count=0;
-	while(*ifs>>page>>user){
-		hash[page].emplace(user);
+bool check_match(const string& s,unordered_map<string,int>& dict,int start,int unit_size,int words_size);
+
+vector<int> find_substrings(const string& s,const vector<string>& words){
+	unordered_map<string,int> dict;
+	for(string word: words){
+		dict[word]++;
 	}
-	for(auto a=hash.begin();a!=hash.end();a++){
-		auto b=a;
-		b++;
-		for(;b!=hash.end();b++){
-			vector<string> intersect_users;
-			set_intersection(a->second.begin(),a->second.end(),
-				b->second.begin(),b->second.end(),back_inserter(intersect_users));
-			if(intersect_users.size()>max_count){
-				max_count=intersect_users.size();
-				res = {a->first,b->first};
-			}
+	vector<int> res;
+	size_t unit_size = words.front().size();
+	for(int i=0;i+unit_size*words.size()<s.size();i++){
+		if(check_match(s,dict,i,unit_size,words.size())){
+			res.emplace_back(i);
 		}
 	}
 	return res;
 }
 
+bool check_match(const string& s,unordered_map<string,int>& dict,int start,int unit_size,int words_size){
+	unordered_map<string,int> cur_dict;
+	for(int i=0;i<words_size;i++){
+		string cur_word = s.substr(start+i*unit_size,unit_size);
+		auto it = dict.find(cur_word);
+		if(it==dict.end()){
+			return false;
+		}
+		cur_dict[cur_word]++;
+		if(cur_dict[cur_word]>dict[cur_word]){
+			return false;
+		}
+	}
+	return true;
+}
+
 int main(){
-	ifstream is("metafile");
-	pair<string,string> res = affinity_pair(&is);
-	cout<<res.first<<" "<<res.second;
+	string input1 = "abcdabcdabcdabcdabcd";
+	vector<string> input2 = {"a","b","c"};
+	vector<int> res = find_substrings(input1,input2);
+	for(int x : res)
+		cout<<x<<" ";
 }
