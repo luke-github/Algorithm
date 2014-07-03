720fb7befb7da0c4c29a3757a691208040065e82
Wen-Qi Cao <wenqicao@gmail.com>
2014-07-03 19:27:49 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -2,11 +2,23 @@
 #include <vector>
 using namespace std;
 
+void delete_duplicate(vector<int>& vec){
+	int cur = 1;
+	int tail= 1;
+	for(;cur<vec.size();cur++){
+		if(vec[cur]==vec[cur-1]){
+			cur++;
+		}else{
+			vec[tail++]=vec[cur++];
+		}
+	}
+}
+
 int main(){
-	vector<int> vec = {1,3,4,2,3,6,7,5,4,3,2,3,4};
-	sort(vec.begin(),vec.end());
-	vec.erase(unique(vec.begin(),vec.end()),vec.end());
-	for(int x:vec){
+	vector<int> input = {1,3,4,5,3,7,4,9,5,6};
+	sort(input.begin(),input.end());
+	delete_duplicate(input);
+	for(int x: input){
 		cout<<x<<" ";
 	}
 }
