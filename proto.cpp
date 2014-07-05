ebf9bfe733e0e1ba770d9e6816dcaa0169a9e69f
Wen-Qi Cao <wenqicao@gmail.com>
2014-07-05 15:08:02 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,44 +1,30 @@
-#include <vector>
 #include <iostream>
+#include <memory>
 using namespace std;
 
-struct Player{
-	int height;
-	bool operator < (const Player& s) const{
-		return height < s.height;
-	}
+template<class T>
+struct ListNode{
+	T data;
+	shared_ptr<ListNode<T>> next;
 };
 
-class Team{
-public:
-	Team(vector<int>& heights){
-		for(int x: heights){
-			team_members_.emplace_back(Player{x});
-		}
-	}
-	bool operator < (const Team& another) const{
-		std::vector<Player> t1(sort_member_func());
-		std::vector<Player> t2(another.sort_member_func());
-		for(int i=0;i<t1.size();i++){
-			if(!(t1[i]<t2[i])){
-				return false;
-			}
-		}
-		return true;
-	}
-private:
-	vector<Player> sort_member_func() const{
-		vector<Player> sorted_array(team_members_);
-		sort(sorted_array.begin(),sorted_array.end());
-		return sorted_array;
-	}
-	vector<Player> team_members_;
-};
+shared_ptr<ListNode<int>> merge_list(shared_ptr<ListNode<int>> l1,shared_ptr<ListNode<int>> l2);
 
-int main(){
-	vector<int> team1 = {1,3,2,4,5,6,8};
-	vector<int> team2 = {11,14,13,12,16,19,15};
-	Team T1(team1);
-	Team T2(team2);
-	cout<<(T1<T2);
+shared_ptr<ListNode<int>> merge_sort_list(shared_ptr<ListNode<int>> head){
+	if(head==nullptr || head->next==nullptr){
+		return head;
+	}
+	shared_ptr<ListNode<int>> fast=head,slow=head,pre_slow=nullptr;
+	while(fast){
+		fast=fast->next;
+		if(fast){
+			pre_slow=slow;
+			fast=fast->next;
+			slow=slow->next;
+		}
+	}
+	pre_slow=nullptr;
+	fast=merge_sort_list(head);
+	slow=merge_sort_list(slow);
+	return merge_list(fast,slow);
 }
