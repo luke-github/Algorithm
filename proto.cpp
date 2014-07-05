ea657ecc07e5cdb3e442fed4ed0012196d7cebee
Wen-Qi Cao <wenqicao@gmail.com>
2014-07-05 17:15:12 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,27 +1,26 @@
 #include <iostream>
 #include <vector>
-#include <numeric>
+#include <fstream>
 using namespace std;
 
-double complete_search(double budget,vector<double>& vec){
-	sort(vec.begin(),vec.end());
-	vector<double> pre_sum;
-	partial_sum(vec.begin(),vec.end(),back_inserter(pre_sum));
-	vector<double> costs;
-	for(int i=0;i<vec.size();i++){
-		costs.emplace_back(pre_sum[i]+vec[i]*(vec.size()-i-1));
+void indirect_sort(const string& filename){
+	ifstream ifs(filename);
+	int x;
+	vector<int> vec;
+	while(ifs>>x){
+		vec.emplace_back(x);
 	}
-	auto it = lower_bound(costs.begin(),costs.end(),budget);
-	if(it==costs.cend()){
-		return -0.1;
-	}else if(it==costs.begin()){
-		return budget/vec.size();
+	vector<int*> point_vec;
+	for(int &x : vec){
+		point_vec.emplace_back(&x);
 	}
-	int index = distance(costs.begin(),it)-1;
-	return vec[index]+(budget - costs[index])/(vec.size()-index-1);
+	sort(point_vec.begin(),point_vec.end(),[](const int* a,const int* b)->bool{return *a<*b;});
+	ofstream ofs("output14_15");
+	for(const int* m : point_vec){
+		ofs<<*m<<endl;
+	}
 }
 
 int main(){
-	vector<double> input = {1,2,3,4,5,6,7,8,9};
-	cout<<complete_search(30,input);
+	indirect_sort("input14_15");
 }
