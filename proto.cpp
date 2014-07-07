c3ab71a59fa095cfb7547292b9a5ed7229c6ce30
Wen-Qi Cao <wenqicao@gmail.com>
2014-07-07 17:14:48 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -10,35 +10,29 @@ struct BST{
 	shared_ptr<BST<T>> right;
 };
 
-shared_ptr<BST<int>> rebuild_BST_handler(vector<int>& vec, int low, int high, int* index){
-	if(vec.size()==*index){
-		return nullptr;
+shared_ptr<BST<int>> build_BSF_handler(vector<int>& vec, int start,int end){
+	if(start<end){
+		int mid = (start+end)>>1;
+		return make_shared<BST<int>>(BST<int>{vec[mid],build_BSF_handler(vec,start,mid),build_BSF_handler(vec,mid+1,end)});
 	}
-	int cur = vec[*index];
-	if(cur<low || cur>high){
-		return nullptr;
-	}
-	++*index;
-	return make_shared<BST<int>>(BST<int>{cur,rebuild_BST_handler(vec,low,cur,index),rebuild_BST_handler(vec,cur,high,index)});
+	return nullptr;
 }
 
-shared_ptr<BST<int>> rebuild_BST(vector<int>& vec){
-	int index=0;
-	return rebuild_BST_handler(vec,1<<31,(1<<31)-1,&index);
+shared_ptr<BST<int>> build_BSF(vector<int>& vec){
+	return build_BSF_handler(vec,0,vec.size());
 }
 
-void pint_result(shared_ptr<BST<int>>& head){
+void print_result(shared_ptr<BST<int>>& head){
 	if(head==nullptr){
 		return;
 	}
 	cout<<head->data<<"->";
-	pint_result(head->left);
-	pint_result(head->right);
+	print_result(head->left);
+	print_result(head->right);
 }
 
-
 int main(){
-	vector<int> input = {5,3,2,4,6};
-	shared_ptr<BST<int>> head = rebuild_BST(input);
-	pint_result(head);
+	vector<int> input = {1,2,3,4,5,6,7,8,9};
+	shared_ptr<BST<int>> res = build_BSF(input);
+	print_result(res);
 }
