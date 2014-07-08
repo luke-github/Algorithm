ef4284f1c1be8166911b4f47f4e6e868b10f5f92
Wen-Qi Cao <wenqicao@gmail.com>
2014-07-08 15:35:27 -0400

    init

---
+++ 15_20.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <memory>
+using namespace std;
+
+template<class T>
+struct BST{
+	T data;
+	shared_ptr<BST<T>> left;
+	shared_ptr<BST<T>> right;
+};
+
+bool min_fist_tree_algorithm(shared_ptr<BST<int>>& head, int k){
+	if(!head||head->data>k){
+		return false;
+	}else if(head->data==k){
+		return true;
+	}else if(head->right&&k>head->right->data){
+		return min_fist_tree_algorithm(head->right,k);
+	}else{
+		return min_fist_tree_algorithm(head->left,k);
+	}
+}
