1058cd128528390fba1150d8f0dd64912f3ece24
Wen-Qi Cao <wenqicao@gmail.com>
2014-07-08 16:44:46 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,26 +1,40 @@
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
 #include <iostream>
-#include <memory>
 using namespace std;
 
-template<class T>
-struct BST{
-	T data;
-	shared_ptr<BST<T>> left;
-	shared_ptr<BST<T>> right;
-};
-
-bool min_first_BSF_check(shared_ptr<BST<int>>& head, int k){
-	if(!head||head->data>k){
+class CreditClient{
+public:
+	bool insert(string name, int value){
+		if(credits_.emplace(name,value - offset_).second){
+			inverse_credits_[value - offset_].emplace(name);
+			return true;
+		}
 		return false;
-	}else if(head->data==k){
-		return true;
-	}else if(head->right&&k>=head->right->data){
-		return min_first_BSF_check(head->right,k);
-	}else{
-		return min_first_BSF_check(head->left,k);
 	}
-}
-
-int main(){
-	
-}
+	bool remove(string name){
+		auto credit_it = credits_.find(name);
+		if(credit_it!=credits_.end()){
+			inverse_credits_[credit_it->second].erase(name);
+			credits_.erase(credit_it);
+			return true;
+		}
+		return false;
+	}
+	int lookup(string name){
+		auto it = credits_.find(name);
+		return it == credits_.end() ? -1 : it->second+offset_;
+	}
+	void addAll(int c){
+		offset_+=c;
+	}
+	string max_value(){
+		auto it = inverse_credits_.crbegin();
+		return it == inverse_credits_.crend() || it->second.empty() ? "" : *it->second.cbegin();
+	}
+private:
+	unordered_map<string,int> credits_;
+	map<int,unordered_set<string>> inverse_credits_;
+	int offset_=0;
+};
