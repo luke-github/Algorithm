674d6edac3f97038cac093098e7b8aabff02145d
Wen-Qi Cao <wenqicao@gmail.com>
2014-07-08 15:37:13 -0400

    init

--- 15_20.cpp
+++ 15_20.cpp
@@ -14,7 +14,7 @@ bool min_fist_tree_algorithm(shared_ptr<BST<int>>& head, int k){
 		return false;
 	}else if(head->data==k){
 		return true;
-	}else if(head->right&&k>head->right->data){
+	}else if(head->right&&k>=head->right->data){
 		return min_fist_tree_algorithm(head->right,k);
 	}else{
 		return min_fist_tree_algorithm(head->left,k);
--- proto.cpp
+++ proto.cpp
@@ -1,4 +1,3 @@
-#include <list>
 #include <iostream>
 #include <memory>
 using namespace std;
@@ -10,20 +9,18 @@ struct BST{
 	shared_ptr<BST<T>> right;
 };
 
-shared_ptr<BST<int>> larger_than(shared_ptr<BST<int>>& head, int k){
-	if(head==nullptr){
-		return nullptr;
-	}else if(head->data>=k){
-		auto node = larger_than(head->left,k);
-		return node?node:head;
+bool min_first_BSF_check(shared_ptr<BST<int>>& head, int k){
+	if(!head||head->data>k){
+		return false;
+	}else if(head->data==k){
+		return true;
+	}else if(head->right&&k>=head->right->data){
+		return min_first_BSF_check(head->right,k);
+	}else{
+		return min_first_BSF_check(head->left,k);
 	}
-	return larger_than(head->right,k);
 }
 
-list<shared_ptr<BST<int>>> range_nodes(shared_ptr<BST<int>>& head, int low, int high){
-	list<shared_ptr<BST<int>>> res;
-	for(auto it = larger_than(head, low);it&&it->data<=high;it=find_next(it)){
-		res.emplace_back(it);
-	}
-	return res;
+int main(){
+	
 }
