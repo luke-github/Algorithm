2995069362378bddf3bd21301964b2ea138da22f
Wen-Qi Cao <wenqicao@gmail.com>
2014-07-09 18:31:26 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -2,35 +2,44 @@
 #include <vector>
 using namespace std;
 
+bool is_palidrome(string& s){
+	for(int i=0,j=s.size()-1;i<j;i++,j--){
+		if(s[i]!=s[j]){
+			return false;
+		}
+	}
+	return true;
+}
 
-void paren_handler(int reminder, int left_paren, string* s, vector<string>* res){
-	if(reminder==0){
-		res->emplace_back(*s);
+void palidrome_partition_handler(string& s,int start,vector<string>* ans,vector<vector<string>>* result){
+	if(start==s.size()){
+		result->emplace_back(*ans);
 		return;
 	}
-	if(left_paren<reminder){
-		s->push_back('(');
-		paren_handler(reminder-1,left_paren+1,s,res);
-		s->pop_back();
-	}
-	if(left_paren>0){
-		s->push_back(')');
-		paren_handler(reminder-1,left_paren-1,s,res);
-		s->pop_back();
+	for(int i=start+1;i<=s.size();i++){
+		string cur = s.substr(start,i-start);
+		if(is_palidrome(cur)){
+			ans->push_back(cur);
+			palidrome_partition_handler(s,i,ans,result);
+			ans->pop_back();
+		}
 	}
 }
 
-
-vector<string> paren(int n){
-	vector<string> res;
-	string s;
-	paren_handler(n<<1,0,&s,&res);
-	return res;
+vector<vector<string>> palidrome_partition(string& s){
+	vector<vector<string>> result;
+	vector<string> ans;
+	palidrome_partition_handler(s,0,&ans,&result);
+	return result;
 }
 
 int main(){
-	auto res = paren(5);
-	for(auto x : res){
-		cout<<x<<endl;
+		string input ="aba";
+	auto res = palidrome_partition(input);
+	for(auto x:res){
+		for(auto y : x){
+			cout<<y<<",";
+		}
+		cout<<endl;
 	}
 }
