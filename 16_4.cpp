cb5352fc55197ae3745657a4740621091cfe0b27
Wen-Qi Cao <wenqicao@gmail.com>
2014-07-09 14:22:58 -0400

    init

---
+++ 16_4.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void permutation_handler(vector<int>* vec, int index, vector<vector<int>>* res){
+	if(index==vec->size()){
+		res->emplace_back(*vec);
+		return;
+	}
+	for(int i=index;i<vec->size();i++){
+		swap((*vec)[i],(*vec)[index]);
+		permutation_handler(vec,index+1,res);
+		swap((*vec)[i],(*vec)[index]);
+	}
+}
+
+vector<vector<int>> permutation(vector<int>& vec){
+	vector<vector<int>> res;
+	permutation_handler(&vec,0,&res);
+	return res;
+}
+
+int main(){
+	vector<int> input = {1,2,3};
+	auto result = permutation(input);
+	for(vector<int> v : result){
+		for(int x : v){
+			cout<<x<<" ";
+		}
+		cout<<endl;
+	}
+}
