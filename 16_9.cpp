ce97b79b3531545d47f23139047e241fbfbd4dad
Wen-Qi Cao <wenqicao@gmail.com>
2014-07-10 14:18:02 -0400

    init

--- 16_9.cpp
+++ 16_9.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <memory>
+#include <vector>
 using namespace std;
 
+template<class T>
+struct BST{
+	T data;
+	shared_ptr<BST<T>> left;
+	shared_ptr<BST<T>> right;
+};
+
+vector<shared_ptr<BST<int>>> generate_all_tree_handler(int start, int end){
+	vector<shared_ptr<BST<int>>> res;
+	if(start>end){
+		res.emplace_back(nullptr);
+		return res;
+	}
+	for(int i=start;i<=end;i++){
+		auto left_res = generate_all_tree_handler(start,i-1);
+		auto right_res = generate_all_tree_handler(i+1,end);
+		for(auto x : left_res){
+			for(auto y : right_res){
+				res.emplace_back(make_shared<BST<int>>(BST<int>{i,x,y}));
+			}
+		}
+	}
+	return res;
+}
+
+vector<shared_ptr<BST<int>>> generate_all_tree(int n){
+	return generate_all_tree_handler(1,n);
+}
+
 int main(){
-	int a = 10;
-	int b(move(a));
-	cout<<b<<endl;
-	cout<<a;
+	auto res = generate_all_tree(5);
+	for(auto x : res){
+		cout<<x->data<<" ";
+	}
 }
