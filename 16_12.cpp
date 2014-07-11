ff8873c71ea417127cd66e9fe5a9ef61f06fa2ea
Wen-Qi Cao <wenqicao@gmail.com>
2014-07-10 22:20:28 -0400

    init

--- 16_12.cpp
+++ 16_12.cpp
@@ -2,20 +2,33 @@
 #include <vector>
 using namespace std;
 
-int combination_algorithm(int k, vector<int>& vec){
-	vector<int> res(k+1,0);
+int combination_without_permutation(int n, vector<int>& vec){
+	vector<int> res(n+1,0);
 	res[0]=1;
-	for(int i=0;i<=k;i++){
-		for(int x: vec){
+	for(int x : vec){
+		for(int i=x;i<=n;i++){
+			res[i] += res[i-x];
+		}
+	}
+	return res[n];
+}
+
+int combination_with_permutation(int n, vector<int>& vec){
+	vector<int> res(n+1,0);
+	res[0]=1;
+	for(int i=0;i<=n;i++){
+		for(int x : vec){
 			if(i>=x){
 				res[i] += res[i-x];
 			}
 		}
 	}
-	return res[k];
+	return res[n];
 }
+
 
 int main(){
 	vector<int> input = {2,3,7};
-	cout<<combination_algorithm(12,input);
+	cout<<combination_without_permutation(12,input)<<endl;
+	cout<<combination_with_permutation(12,input)<<endl;
 }
