3df7077529c141b97bf7562f84f0dca9bdc33298
Wen-Qi Cao <wenqicao@gmail.com>
2014-07-12 18:31:02 -0400

    init

---
+++ 17_10.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <iostream>
+#include <unordered_set>
+#include <numeric>
+using namespace std;
+
+int minimal_difference(vector<int>& vec){
+	int total = accumulate(vec.begin(),vec.end(),0);
+	unordered_set<int> dp;
+	dp.emplace(0);
+	for(int x : vec){
+		for(int i=total>>1;i>=x;i--){
+			if(dp.find(i-x)!=dp.end()){
+				dp.emplace(i);
+			}
+		}
+	}
+	for(int i=(total>>1);i>=0;i--){
+		if(dp.find(i)!=dp.end()){
+			return total-i-i;
+		}
+	}
+	return total;
+}
+
+int main(){
+	vector<int> input = {1,5,3,4};
+	cout<<minimal_difference(input);
+}
