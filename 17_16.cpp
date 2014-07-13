2997dcaa7dab61975f8e2ee72c4b3baf93ad004e
Wen-Qi Cao <wenqicao@gmail.com>
2014-07-13 17:53:47 -0400

    init

---
+++ 17_16.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+using namespace std;
+
+int ways_parlidrome(string& s){
+	vector<vector<bool>> checker(s.size(),vector<bool>(s.size(),false));
+	vector<int> dp(s.size()+1);
+	iota(dp.rbegin(),dp.rend(),0);
+	for(int i=s.size()-1;i>=0;i--){
+		for(int j=i;j<s.size();j++){
+			if(s[i]==s[j]&&(j-i<2||checker[i+1][j-1])){
+				checker[i][j]=true;
+				dp[i]=min(dp[i],1+dp[j+1]);
+			}
+		}
+	}
+	return dp[0];
+}
+
+int main(){
+	string input = "acad";
+	cout<<ways_parlidrome(input);
+}
