4a09f8e5cb50353119af5e834350be6679f0c94f
Wen-Qi Cao <wenqicao@gmail.com>
2014-07-13 21:47:56 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -3,23 +3,23 @@
 #include <numeric>
 using namespace std;
 
-int ways_parlidrom_algorithm(string& s){
+int ways_parlidrome_algorithm(const string& s){
 	vector<vector<bool>> checker(s.size(),vector<bool>(s.size(),false));
 	vector<int> dp(s.size()+1);
-	iota(dp.rbegin(),dp.rend(),0);
-	for(int i=s.size()-1;i>=0;i--){
-		for(int j=i;j<s.size();j++){
-			if(s[i]==s[j]&&(j-i<2||checker[i+1][j-1])){
-				checker[i][j]=true;
-				dp[i] = min(dp[i],1+dp[j+1]);
+	iota(dp.begin(),dp.end(),0);
+	for(int i=0;i<s.size();i++){
+		for(int j=i;j>=0;j--){
+			if(s[i]==s[j]&&(i-j<2||checker[j+1][i-1])){
+				checker[j][i]=true;
+				dp[i] = min(dp[i],1+dp[j-1]);
 			}
 		}
 	}
-	return dp[0];
+	return dp[s.size()-1];
 }
 
 
 int main(){
-	string input = "acad";
-	cout<<ways_parlidrom_algorithm(input);
+	string input = "ad";
+	cout<<ways_parlidrome_algorithm(input);
 }
