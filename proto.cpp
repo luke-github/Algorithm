b39110e02fabbbd725aae9818d0bbd75385f33d2
Wen-Qi Cao <wenqicao@gmail.com>
2014-07-15 16:39:49 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -2,54 +2,36 @@
 #include <vector>
 using namespace std;
 
-struct point{
-	bool operator == (const point& that) const {
-		return x==that.x && y==that.y;
-	}
-	int x,y;
-};
-
-bool is_ok(point m, vector<vector<int>>& maze){
-	return m.x>=0 && m.x<maze.size() && m.y >=0 && m.y<maze[m.x].size() && maze[m.x][m.y]==0;
-}
-
-bool maze_algorithm_handler(point& cur, point end, vector<vector<int>>* maze, vector<point>* path){
-	if(cur==end){
-		return true;
-	}
-	int shift[4][2]={{0,1},{0,-1},{1,0},{-1,0}};
+void flip_algorithm(vector<vector<bool>>* vec, int x, int y){
+	bool color = (*vec)[x][y];
+	(*vec)[x][y] = !(*vec)[x][y];
+	int shift[4][2] = {{0,1},{0,-1},{1,0},{-1,0}};
 	for(auto m : shift){
-		point next={cur.x+m[0],cur.y+m[1]};
-		if(is_ok(next,*maze)){
-			path->emplace_back(next);
-			(*maze)[next.x][next.y]=1;
-			if(maze_algorithm_handler(next,end,maze,path)){
-				return true;
-			}
-			path->pop_back();
+		int nx = x+m[0],ny=y+m[1];
+		if(nx>=0 && nx<vec->size() && ny>=0 && ny<(*vec)[nx].size() && (*vec)[nx][ny]==color){
+			flip_algorithm(vec,nx,ny);
 		}
 	}
-	return false;
-}
-
-vector<point> maze_algorithm(vector<vector<int>> maze, point start, point end){
-	vector<point> path;
-	maze[start.x][start.y]=1;
-	path.emplace_back(start);
-	if(!maze_algorithm_handler(start,end,&maze,&path)){
-		path.pop_back();
-	}
-	return path;
 }
 
 int main(){
-	vector<vector<int>> input(10,vector<int>(10,0));
-	point s = {0,0};
-	point e = {9,9};
-	input[1][9]=1;
-	input[8][9]=1;
-	auto res= maze_algorithm(input,s,e);
-	for(auto m : res){
-		cout<<m.x<<"."<<m.y<<" ";
+	vector<vector<bool>> input(5,vector<bool>(5,0));
+	input[0][1]=1;
+	input[0][0]=1;
+	input[1][0]=1;
+	input[1][1]=1;
+	for(auto x: input){
+		for(auto y : x){
+			cout<<y<<" ";
+		}
+		cout<<endl;
+	}
+	cout<<endl;
+	flip_algorithm(&input,3,3);
+		for(auto x: input){
+		for(auto y : x){
+			cout<<y<<" ";
+		}
+		cout<<endl;
 	}
 }
