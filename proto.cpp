aa104b8ceaa4f48199387bc119601e331b7f927d
Wen-Qi Cao <wenqicao@gmail.com>
2014-07-16 14:58:46 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,32 +1,40 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <unordered_map>
 using namespace std;
 
-struct Node{
-	int value;
-	vector<Node*> edges;
+struct GraphVertex{
+	int d = -1;
+	vector<GraphVertex*> edges;
 };
 
-Node* clone_graph(Node* G){
-	if(!G){
-		return nullptr;
-	}
-	unordered_map<Node*,Node*> hash;
-	queue<Node*> q;
-	hash.emplace(G,new Node({G->value}));
-	q.emplace(G);
+bool BSF_search(GraphVertex* s){
+	queue<GraphVertex*> q;
+	q.emplace(s);
 	while(!q.empty()){
-		Node* cur = q.front();
-		q.pop();
-		for(auto x : cur->edges){
-			if(hash.find(x)==hash.end()){
-				hash.emplace(x,new Node({x->value}));
-				q.emplace(x);
+		for(GraphVertex* m : q.front()->edges){
+			if(m->d==-1){
+				m->d = q.front()->d +1;
+				q.emplace(m);
+			}else{
+				if(m->d==q.front()->d){
+					return false;
+				}
 			}
-			hash[cur]->edges.emplace_back(hash[x]);
+		}
+		q.pop();
+	}
+	return true;
+}
+
+bool is_feasible(vector<GraphVertex>* G){
+	for(GraphVertex s : *G){
+		if(s.d==-1){
+			s.d=0;
+			if(!BSF_search(&s)){
+				return false;
+			}
 		}
 	}
-	return hash[G];
+	return true;
 }
