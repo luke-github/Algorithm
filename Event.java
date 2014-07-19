dfb8363659fa0976f8dd2fec1e85926c14e64592
Wen-Qi Cao <wenqicao@gmail.com>
2014-07-19 13:43:54 -0400

    init

--- Event.java
+++ Event.java
@@ -1,9 +1,37 @@
-public class Event {
 
-    public static void main(String[] args) {
+class oddeventMonitor{
+	public static final boolean odd_turn = true;
+	public static final boolean even_turn = false;
+	private boolean turn = odd_turn;
+	
+	public synchronized void waitTurn(boolean oldTurn){
+		while(turn!=oldTurn){
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public synchronized void toggleTurn(){
+		turn ^= true;
+		notify();
+	}
+}
 
-        System.out.println("Goodbye, World!");
-
-    }
-
+class oddThread extends Thread{
+	private oddeventMonitor monitor;
+	public oddThread(oddeventMonitor monitor){
+		this.monitor = monitor;
+	}
+	@Override
+	public void run(){
+		for(int i=1;i<100;i=i+2){
+			monitor.waitTurn(oddeventMonitor.odd_turn);
+			System.out.println(i);
+			monitor.toggleTurn();
+		}
+	}
 }
