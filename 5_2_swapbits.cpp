fb3e83f9153074a86a1d23743fd25cc0d0164458
Wen-Qi Cao <wenqicao@gmail.com>
2014-07-22 16:06:13 -0400

    init

--- 5_2_swapbits.cpp
+++ 5_2_swapbits.cpp
@@ -1,14 +1,12 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-long swapPosition(long x, int i,int j){
+int swap_bit(int x, int i, int j){
 	if(((x>>i)&1)!=((x>>j)&1)){
-		x ^= ((1l<<i) | (1l<<j));
+		x ^= (1<<i) | (1<<j);
 	}
 	return x;
 }
-
 int main(){
-	cout<<swapPosition(42,2,3)<<endl;
-	cout<<swapPosition(10,1,2)<<endl;
+	cout<<swap_bit(18,2,4);
 }
