c3ce8f3582b74933d22218257898b59ea3e250a3
Wen-Qi Cao <wenqicao@gmail.com>
2014-07-22 15:27:21 -0400

    init

--- 5_1_parity.cpp
+++ 5_1_parity.cpp
@@ -1,27 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int checkParity(int n){
+bool parity_algorithm(int x){
 	int result = 0;
-	while(n){
-		result ^= (1&n);
-		n >>= 1;
-	}
-	return result;
-}
-
-// some performance improvement in this function
-int checkParity2(int n){
-	int result = 0;
-	while(n){
+	while(x){
 		result ^= 1;
-		n &= (n-1);
+		x &= (x-1);
 	}
 	return result;
 }
 
 int main(){
-	int testnum = 5;
-	cout<<checkParity(testnum)<<endl;
-	cout<<checkParity2(testnum)<<endl;
+	cout<<parity_algorithm(4);
 }
