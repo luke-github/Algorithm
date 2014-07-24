5e7cf9124739cfaef3449bf0e72ad491bd1c48b3
Wen-Qi Cao <wenqicao@gmail.com>
2014-07-24 15:02:08 -0400

    init

---
+++ 5_7.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+double pow_algorithm(double x, int y){
+	double res = 1.0;
+	long long power = y;
+	if(y<0){
+		x = 1.0/x;
+		power = -power;
+	}
+	while(power){
+		if(power&1){
+			res *= x;
+		}
+		x *= x, power>>=1;
+	}
+	return res;
+}
+int main(){
+	cout<<pow_algorithm(2,-5);
+}
