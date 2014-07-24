4e493d938bc28a0025c82339bb5c3bc3513ec0f1
Wen-Qi Cao <wenqicao@gmail.com>
2014-07-24 15:52:27 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
 using namespace std;
 
-double pow_algorithm(double x, int y){
-	double res = 1.0;
-	long long power = y;
-	if(y<0){
-		x = 1.0/x;
-		power = -power;
+string change_base(const string& s, int b1, int b2){
+	bool neg = s.front()=='-';
+	int x;
+	for(int i = neg?1:0;i<s.size();i++){
+		x *= b1;
+		x += isdigit(s[i])?s[i]-'0':s[i]-'A'+10;
 	}
-	while(power){
-		if(power&1){
-			res*=x;
-		}
-		x*=x;
-		power>>=1;
+	string res;
+	while(x){
+		int r = x%b2;
+		res.push_back(r>=10?'A'+r-10:r+'0');
+		x /= b2;
 	}
+	if(neg){
+		res.push_back('-');
+	}
+	reverse(res.begin(),res.end());
 	return res;
 }
 
 int main(){
-	cout<<pow_algorithm(2,10);
+	string input = "B";
+	cout<<change_base(input,16,10);
 }
