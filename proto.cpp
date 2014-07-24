8957cf74b1ef2581b5a93c3d591106a4ae3f901f
Wen-Qi Cao <wenqicao@gmail.com>
2014-07-24 17:44:02 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,15 +1,25 @@
+#include <complex>
 #include <iostream>
 using namespace std;
 
-int reverse_algorithm(int n){
-	int res = 0;
-	while(n){
-		res =  res*10 + n%10;
-		n /=10;
+bool checker_parlidrome(int x){
+	if(x<0){
+		return false;
 	}
-	return res;
+	int num_digits = floor(log10(x))+1;
+	int temp_x = x;
+	int shift = pow(10,num_digits-1);
+	for(int i=0;i<(num_digits>>1);i++){
+		if(x/shift != temp_x%10){
+			return false;
+		}
+		x %= shift;
+		shift /=10;
+		temp_x /=10;
+	}
+	return true;
 }
 
 int main(){
-	cout<<reverse_algorithm(12345);
+	cout<<checker_parlidrome(1333333331);
 }
