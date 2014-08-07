dc56df886354b9045f28f8fb9e78c384b4a6c5c4
Wen-Qi Cao <wenqicao@gmail.com>
2014-08-07 16:32:38 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int divide_func(int x, int y){
-	int res = 0;
-	while(x>=y){
-		int power = 1;
-		while((y<<power)>=(y<<(power-1))&&(y<<power<=x)){
-			power++;
-		}
-		res += 1<<(power-1);
-		x -= y<<(power-1);
+int GCD(int x, int y){
+	if(x==0){
+		return y;
+	}else if(y==0){
+		return x;
+	}else if(!(x&1)&&!(y&1)){
+		return GCD(x>>1,y>>1)<<1;
+	}else if(!(x&1)&&(y&1)){
+		return GCD(x>>1,y);
+	}else if((x&1)&&!(y&1)){
+		return GCD(x,y>>1);
+	}else if(x>y){
+		return GCD(x-y,y);
+	}else{
+		return GCD(x,y-x);
 	}
-	return res;
 }
 
 int main(){
-	cout<<divide_func(8,3);
+	cout<<GCD(10,5);
 }
