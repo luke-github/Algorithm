af61831f4001e334c93dfa3cb666eb16dd80e44f
Wen-Qi Cao <wenqicao@gmail.com>
2014-08-07 16:29:47 -0400

    init

---
+++ 5_15.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int GCD(int x, int y){
+	if(x==0){
+		return y;
+	}else if(y==0){
+		return x;
+	}else if(!(x&1)&&!(y&1)){
+		return GCD(x>>1,y>>1)<<1;
+	}else if(!(x&1)&&(y&1)){
+		return GCD(x>>1,y);
+	}else if((x&1)&&!(y&1)){
+		return GCD(x,y>>1);
+	}else if(x>y){
+		return GCD(x-y,y);
+	}else{
+		return GCD(x,y-x);
+	}
+}
+
+int main(){
+	cout<<GCD(14,12);
+}
