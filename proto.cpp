8887e826795980799094f31e893028fda2cc7577
Wen-Qi Cao <wenqicao@gmail.com>
2014-08-07 18:11:10 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
+#include <numeric>
+#include <vector>
 using namespace std;
 
-int GCD(int x, int y){
-	if(x==0){
-		return y;
-	}else if(y==0){
-		return x;
-	}else if(!(x&1)&&!(y&1)){
-		return GCD(x>>1,y>>1)<<1;
-	}else if(!(x&1)&&(y&1)){
-		return GCD(x>>1,y);
-	}else if((x&1)&&!(y&1)){
-		return GCD(x,y>>1);
-	}else if(x>y){
-		return GCD(x-y,y);
-	}else{
-		return GCD(x,y-x);
+int max_product_fun(const vector<int>& vec){
+	vector<int> L,R(vec.size());
+	partial_sum(vec.cbegin(),vec.cend(),back_inserter(L),multiplies<int>());
+	partial_sum(vec.crbegin(),vec.crend(),R.rbegin(),multiplies<int>());
+	int max_val = 1<<31;
+	for(int i=0;i<vec.size();i++){
+		int forward = i>0?L[i-1]:1;
+		int backward = i+1<vec.size()?R[i+1]:1;
+		max_val = max(max_val,forward*backward);
 	}
+	return max_val;
 }
 
 int main(){
-	cout<<GCD(10,5);
+	vector<int> input = {1,2,3,4};
+	// cout<<max_product_fun(input);
+	vector<int> res;
+	partial_sum(input.begin(),input.end(),back_inserter(res),multiplies<int>());
+	for(int x : res){
+		cout<<x<<" ";
+	}
+
 }
