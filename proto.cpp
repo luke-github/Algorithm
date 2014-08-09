e8513eae4833ce24278f2a74c723ac102d14199f
Wen-Qi Cao <wenqicao@gmail.com>
2014-08-09 14:13:14 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -2,46 +2,17 @@
 #include <vector>
 using namespace std;
 
-
-void apply_sequence_fun(vector<int>* perm, vector<int>* vec){
-	for(int i=0;i<vec->size();i++){
-		if((*perm)[i]>=0){
-			int a = i;
-			int tmp = (*vec)[a];
-			do{
-				int next_a = (*perm)[a];
-				int next_tmp = (*vec)[next_a];
-				(*vec)[next_a] = tmp;
-				(*perm)[a]-=vec->size();
-				a = next_a;
-				tmp = next_tmp;
-			}while(a!=i);
-		}
-	}
-}
-
-void apply_fun(vector<int>* perm, vector<int>* vec){
-	for(int i=0;i<vec->size();i++){
-		if((*perm)[i]>=0){
-			int a = i;
-			int temp = (*vec)[i];
-			do{
-				int next_a = (*perm)[a];
-				int next_temp = (*vec)[next_a];
-				(*vec)[next_a] = temp;
-				(*perm)[a] -= perm->size();
-				a = next_a;
-				temp = next_temp;
-			}while(a!=i);
-		}
-	}
+void rotate_array(int i, vector<int>* vec){
+	i = i % vec->size();
+	reverse(vec->begin(),vec->end());
+	reverse(vec->begin(),vec->begin()+i);
+	reverse(vec->begin()+i,vec->end());
 }
 
 int main(){
-	vector<int> seq = {2,1,0,3};
-	vector<int> input = {11,12,13,14};
-	apply_fun(&seq,&input);
-	for(auto x : input){
+		vector<int> input = {1,2,3,4,5};
+	rotate_array(2,&input);
+	for(auto x: input){
 		cout<<x<<" ";
 	}
 }
