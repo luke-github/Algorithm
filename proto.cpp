ac460c5c70dba4806059c8597e7becbb97f054ac
Wen-Qi Cao <wenqicao@gmail.com>
2014-08-13 15:34:47 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,44 +1,27 @@
 #include <iostream>
-#include <memory>
+#include <stack>
 using namespace std;
 
-template<class T>
-struct ListNode{
-	T data;
-	shared_ptr<ListNode<T>> next;
-	shared_ptr<ListNode<T>> jump;
-};
-
-shared_ptr<ListNode<int>> copy_list(shared_ptr<ListNode<int>>& head){
-	if(!head){
-		return nullptr;
-	}
-	// 1st we need copy node
-	shared_ptr<ListNode<int>> p = head;
-	while(p->next){
-		shared_ptr<ListNode<int>> temp = make_shared<ListNode<int>>(ListNode<int>{p->data,p->next,nullptr});
-		p->next = temp;
-		p = temp->next;
-	}
-	// 2nd copy jump link
-	p = head;
-	while(p){
-		if(p->jump){
-			p->next->jump = p->jump->next;
+int longest_match(const string& s){
+	int max_len=0, last_end=0;
+	stack<int> st;
+	for(int i=0;i<s.size();i++){
+		if(s[i]=='('){
+			st.emplace(i);
+		}else{
+			if(st.empty()){
+				last_end = i+1;
+			}else{
+				st.pop();
+				int start = st.empty()? last_end-1: st.top();
+				max_len = max(max_len,i-start);
+			}
 		}
-		p = p->next->next;
 	}
-	// restore next link
-	p = head;
-	shared_ptr<ListNode<int>> res = p->next;
-	while(p->next){
-		shared_ptr<ListNode<int>> tmp = p->next;
-		p->next = tmp->next;
-		p=tmp;
-	}
-	return res;
+	return max_len;
 }
 
 int main(){
-	
+	string input = "())()()";
+	cout<<longest_match(input);
 }
