dc7f0b12993872752cb8e6843c33d6aebe9f7d1f
Wen-Qi Cao <wenqicao@gmail.com>
2014-08-13 17:37:34 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,27 +1,25 @@
 #include <iostream>
-#include <stack>
+#include <vector>
+#include <queue>
 using namespace std;
 
-int longest_match(const string& s){
-	int max_len=0, last_end=0;
-	stack<int> st;
-	for(int i=0;i<s.size();i++){
-		if(s[i]=='('){
-			st.emplace(i);
-		}else{
-			if(st.empty()){
-				last_end = i+1;
-			}else{
-				st.pop();
-				int start = st.empty()? last_end-1: st.top();
-				max_len = max(max_len,i-start);
-			}
-		}
+struct TrafficElement{
+	bool operator < (const TrafficElement& that) const {
+		return volume < that.volume;
 	}
-	return max_len;
-}
+	bool operator == (const TrafficElement& that) const {
+		return volume ==  that.volume;
+	}
+	int time,volume;
+};
 
-int main(){
-	string input = "())()()";
-	cout<<longest_match(input);
+void max_traffic_func(vector<TrafficElement>& vec,int w){
+	queue<TrafficElement> q;
+	for(int i=0;i<vec.size();i++){
+		q.emplace(vec[i]);
+		while(vec[i].time - q.front().time > w){
+			q.dequeue();
+		}
+		cout<<q.max().volume<<endl;
+	}
 }
