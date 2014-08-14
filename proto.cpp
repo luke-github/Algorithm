3ac93485fa28edc3a382c8938131d08c922bd21e
Wen-Qi Cao <wenqicao@gmail.com>
2014-08-14 15:37:30 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,6 +1,4 @@
-#include <stack>
 #include <iostream>
-#include <vector>
 #include <memory>
 using namespace std;
 
@@ -11,27 +9,45 @@ struct BTN{
 	shared_ptr<BTN<T>> right;
 };
 
-vector<int> preorder_iteration(shared_ptr<BTN<int>>& root){
-	if(!root){
-		return {};
-	}
-	stack<shared_ptr<BTN<int>>> s;
-	vector<int> res;
-	s.emplace(root);
-	while(!s.empty()){
-		auto cur = s.top();
-		s.pop();
-		res.emplace_back(cur->data);
-		if(cur->right){
-			s.emplace(cur->right);
+void left_algo_func(shared_ptr<BTN<int>>& node, bool is_boundary){
+	if(node){
+		if(is_boundary || (!node->left&&!node->right)){
+			cout<<node->data<<" ";
 		}
-		if(cur->left){
-			s.emplace(cur->left);
+		left_algo_func(node->left,is_boundary);
+		left_algo_func(node->right,is_boundary&&!node->left);
+	}
+}
+
+void right_algo_func(shared_ptr<BTN<int>>& node, bool is_boundary){
+	if(node){
+		right_algo_func(node->left,is_boundary&&!node->right);
+		right_algo_func(node->right,is_boundary);
+		if(is_boundary || (!node->left&&!node->right)){
+			cout<<node->data<<" ";
 		}
 	}
-	return res;
+}
+
+void print_exterior(shared_ptr<BTN<int>>& root){
+	if(root){
+		cout<<root->data<<" ";
+		left_algo_func(root->left,true);
+		right_algo_func(root->right,true);
+	}
 }
 
 int main(){
-	
+	shared_ptr<BTN<int>> T1 = make_shared<BTN<int>>(BTN<int>{1,nullptr,nullptr});
+	shared_ptr<BTN<int>> T2 = make_shared<BTN<int>>(BTN<int>{2,nullptr,nullptr});
+	shared_ptr<BTN<int>> T3 = make_shared<BTN<int>>(BTN<int>{3,nullptr,nullptr});
+	shared_ptr<BTN<int>> T4 = make_shared<BTN<int>>(BTN<int>{4,nullptr,nullptr});
+	shared_ptr<BTN<int>> T5 = make_shared<BTN<int>>(BTN<int>{5,nullptr,nullptr});
+	shared_ptr<BTN<int>> T6 = make_shared<BTN<int>>(BTN<int>{6,nullptr,nullptr});
+	T1->left=T2;
+	T1->right=T3;
+	T2->left=T4;
+	T2->right=T5;
+	T3->left=T6;
+	print_exterior(T1);
 }
