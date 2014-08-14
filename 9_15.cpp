ae15b54aeecb9a4938f96e5c98215a883b803488
Wen-Qi Cao <wenqicao@gmail.com>
2014-08-13 20:33:04 -0400

    init

--- 9_15.cpp
+++ 9_15.cpp
@@ -3,8 +3,26 @@
 #include <list>
 using namespace std;
 
-int main(){
-	list<int> a = {1,2,3};
-	for(int x : a)
-		cout<<x<<" ";
+list<int> minimal_steps(int n){
+	if(n==1){
+		return {1};
+	}
+	queue<list<int>> exp_lists;
+	exp_lists.emplace(1,1);
+	while(!exp_lists.empty()){
+		list<int> cur_exp = exp_lists.front();
+		exp_lists.pop();
+		for(int x: cur_exp){
+			int sum = x+cur_exp.back();
+			if(sum>n){
+				break;
+			}
+			list<int> new_exp(cur_exp);
+			new_exp.emplace_back(sum);
+			if(sum==n){
+				return new_exp;
+			}
+			exp_lists.emplace(new_exp);
+		}
+	}
 }
