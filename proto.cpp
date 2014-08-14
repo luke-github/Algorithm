dd6732a43556cab426f224b1ede9bd8c9b665251
Wen-Qi Cao <wenqicao@gmail.com>
2014-08-14 16:32:40 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,53 +1,26 @@
 #include <iostream>
-#include <memory>
+#include <vector>
 using namespace std;
 
-template<class T>
-struct BTN{
-	T data;
-	shared_ptr<BTN<T>> left;
-	shared_ptr<BTN<T>> right;
-};
-
-void left_algo_func(shared_ptr<BTN<int>>& node, bool is_boundary){
-	if(node){
-		if(is_boundary || (!node->left&&!node->right)){
-			cout<<node->data<<" ";
-		}
-		left_algo_func(node->left,is_boundary);
-		left_algo_func(node->right,is_boundary&&!node->left);
-	}
-}
-
-void right_algo_func(shared_ptr<BTN<int>>& node, bool is_boundary){
-	if(node){
-		right_algo_func(node->left,is_boundary&&!node->right);
-		right_algo_func(node->right,is_boundary);
-		if(is_boundary || (!node->left&&!node->right)){
-			cout<<node->data<<" ";
+vector<int> fair_bonus(const vector<int>& vec){
+	vector<int> res(vec.size(),1);
+	for(int i=0;i<vec.size()-1;i++){
+		if(vec[i+1]>vec[i]){
+			res[i+1]=res[i]+1;
 		}
 	}
-}
-
-void print_exterior(shared_ptr<BTN<int>>& root){
-	if(root){
-		cout<<root->data<<" ";
-		left_algo_func(root->left,true);
-		right_algo_func(root->right,true);
+	for(int i=vec.size()-2;i>=0;i--){
+		if(vec[i]>vec[i+1]){
+			res[i] = max(res[i],res[i+1]+1);
+		}
 	}
+	return res;
 }
 
 int main(){
-	shared_ptr<BTN<int>> T1 = make_shared<BTN<int>>(BTN<int>{1,nullptr,nullptr});
-	shared_ptr<BTN<int>> T2 = make_shared<BTN<int>>(BTN<int>{2,nullptr,nullptr});
-	shared_ptr<BTN<int>> T3 = make_shared<BTN<int>>(BTN<int>{3,nullptr,nullptr});
-	shared_ptr<BTN<int>> T4 = make_shared<BTN<int>>(BTN<int>{4,nullptr,nullptr});
-	shared_ptr<BTN<int>> T5 = make_shared<BTN<int>>(BTN<int>{5,nullptr,nullptr});
-	shared_ptr<BTN<int>> T6 = make_shared<BTN<int>>(BTN<int>{6,nullptr,nullptr});
-	T1->left=T2;
-	T1->right=T3;
-	T2->left=T4;
-	T2->right=T5;
-	T3->left=T6;
-	print_exterior(T1);
+		vector<int> input = {300,400,500,200};
+	auto res = fair_bonus(input);
+	for(int x : res){
+		cout<<x<<" ";
+	}
 }
