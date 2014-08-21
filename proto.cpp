a119e83976c08d4a2d5edfc81eb2315ad6338220
Wen-Qi Cao <wenqicao@gmail.com>
2014-08-21 19:17:05 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,33 +1,30 @@
 #include <iostream>
-#include <sstream>
 #include <vector>
-#include <list>
+#include <limits>
 #include <unordered_map>
 using namespace std;
 
-pair<int,int> find_smallest_subarray(istringstream* sin, const vector<string>& q){
-	list<int> loc;
-	unordered_map<string,list<int>::iterator> dict;
-	for(const string& s: q){
-		dict.emplace(s,loc.end());
+pair<int,int> sequentially_subarray(const vector<string>& Q, const vector<string>& A){
+	unordered_map<string, int> K;
+	vector<int> L(Q.size(),-1), D(Q.size(),numeric_limits<int>::max());
+	for(int i=0;i<Q.size();i++){
+		K.emplace(Q[i],i);
 	}
-	int index = 0;
-	pair<int,int> res(-1,-1);
-	string s;
-	while(*sin>>s){
-		auto it = dict.find(s);
-		if(it != dict.end()){
-			if(it->second != loc.end()){
-				loc.erase(it->second);
+	pair<int,int> res(-1,A.size());
+	for(int i=0;i<A.size();i++){
+		auto it = K.find(A[i]);
+		if(it != K.end()){
+			if(it->second==0){
+				D[0]=1;
 			}
-			loc.emplace_back(index);
-			it->second = --loc.end();
+			else if(D[it->second - 1]!=numeric_limits<int>::max()){
+				D[it->second] = i - L[it->second -1] + D[it->second -1];
+			}
+			L[it->second] = i;
+			if(it->second == Q.size() && D.back()< res.second - res.first +1){
+				res = {i - D.back() +1, i};
+			}
 		}
-		// check if program finishes
-		if(loc.size()==q.size()&& ((res.first==-1 && res.second==-1) || (index - loc.front()< res.second - res.first))){
-			res = {loc.front(),index};
-		}
-		index++;
 	}
 	return res;
 }
