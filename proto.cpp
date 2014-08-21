190c82adf13676b5432fb62dec4eecd1f2736d4f
Wen-Qi Cao <wenqicao@gmail.com>
2014-08-21 15:36:03 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
-#include <cmath>
+#include <sstream>
+#include <vector>
+#include <list>
+#include <unordered_map>
 using namespace std;
 
-int main(){
-	int val = GCD(10,4);
-	cout<<val;
+pair<int,int> find_smallest_subarray(istringstream* sin, const vector<string>& q){
+	list<int> loc;
+	unordered_map<string,list<int>::iterator> dict;
+	for(const string& s: q){
+		dict.emplace(s,loc.end());
+	}
+	int index = 0;
+	pair<int,int> res(-1,-1);
+	string s;
+	while(*sin>>s){
+		auto it = dict.find(s);
+		if(it != dict.end()){
+			if(it->second != loc.end()){
+				loc.erase(it->second);
+			}
+			loc.emplace_back(index);
+			it->second = --loc.end();
+		}
+		// check if program finishes
+		if(loc.size()==q.size()&& ((res.first==-1 && res.second==-1) || (index - loc.front()< res.second - res.first))){
+			res = {loc.front(),index};
+		}
+		index++;
+	}
+	return res;
 }
