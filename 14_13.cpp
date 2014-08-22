0f179bb66fc8cd0da6bbb451fc831d09d9ee6965
Wen-Qi Cao <wenqicao@gmail.com>
2014-08-22 14:43:02 -0400

    init

---
+++ 14_13.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int unconstructible_number(vector<int>& vec){
+	sort(vec.begin(),vec.end());
+	int sum = 0;
+	for(int x : vec){
+		if(x > sum+1){
+			break;
+		}
+		sum+=x;
+	}
+	return sum+1;
+}
+
+int main(){
+	vector<int> input = {1,1,5,10};
+	cout<<unconstructible_number(input);
+}
