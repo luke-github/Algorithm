7b9b3bab237176074358032de085387dcbed9243
Wen-Qi Cao <wenqicao@gmail.com>
2014-08-21 21:26:26 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
 using namespace std;
+
+int longest_distinct_subarray(const vector<int>& vec){
+	unordered_map<int,int> last_occurrence;
+	size_t first_index=0, ans=0;
+	for(int i=0;i<vec.size();i++){
+		auto res(last_occurrence.emplace(vec[i],i));
+		if(!res.second){
+			if(res.first->second >= first_index){
+				ans = max(ans,i - first_index);
+				first_index = res.first->second+1;
+			}
+			res.first->second = i;
+		}
+	}
+	ans = max(ans,vec.size()-first_index);
+	return ans;
+}
+
+
 int main(){
-	int a = max(3,5);
-	cout<<a;
+	vector<int> input = {1,2,3,1,2,3,4,5};
+	cout<<longest_distinct_subarray(input);
 }
