f99753940f8551ef3dc097614f40cb762a7e4fea
Wen-Qi Cao <wenqicao@gmail.com>
2014-08-27 14:43:52 -0400

    init

---
+++ 17_13.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int max_floor_handler(int c, int d, vector<vector<int>>* F){
+	if(c==0 || d==0){
+		return 0;
+	}else if(c==1){
+		return d;
+	}
+	if((*F)[c][d]==-1){
+		(*F)[c][d] = max_floor_handler(c-1,d-1,F)+1+max_floor_handler(c,d-1,F);
+	}
+	return (*F)[c][d];
+}
+
+int max_floor(int c, int d){
+	vector<vector<int>> F(c+1,vector<int>(d+1,-1));
+	return max_floor_handler(c,d,&F);
+}
+
+int main(){
+	cout<<max_floor(3,4);
+}
